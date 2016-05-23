d56865e2c649ec7fdde67ec460f236abb0abbbef
Vesa Ojalehto <vesa.ojalehto@finnopt.com>
2016-05-23 16:35:04 +0200

    Use new tui for AuxiliaryServices problem

--- pyDESDEO/examples/AuxiliaryServices.py
+++ pyDESDEO/examples/AuxiliaryServices.py
@@ -30,111 +30,35 @@ References
 
 '''
 import sys,os
-import logging
+
 example_path=os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(example_path,".."))
 
+from utils import tui
 
-import preference
-from utils.tui import *
-from preference.PreferenceInformation import DirectSpecification
-from problem.Problem import PreGeneratedProblem
 from method.NAUTILUS import NAUTILUSv1,ENAUTILUS
-from preference.PreferenceInformation import DirectSpecification, RelativeRanking
-from optimization.OptimizationMethod import SciPy, SciPyDE,PointSearch
+
+from optimization.OptimizationMethod import PointSearch
+from problem.Problem import PreGeneratedProblem
 
 from utils.misc import Logger 
 sys.stdout = Logger(os.path.splitext(os.path.basename(__file__))[0])
 
-
-def select_iter(method,default=1):
-    method.printCurrentIteration()
-    try:
-        text = prompt(u'Select iteration point Ns: ',validator=IterValidator(method))
-    except TypeError:
-        text=str(default)
-    if text=="q":
-        sys.exit("User Exit")
-    elif text=="c":
-        return None
-    return method.zhs[int(text)-1],method.zh_los[int(text)-1]
-
-
 if __name__ == '__main__':
-    if "--debug" in sys.argv:
-        FORMAT = "%(message)s"
-        logging.basicConfig(format=FORMAT,level=logging.DEBUG)
-
-    
+    # SciPy breaks box constraints
     method = ENAUTILUS(PreGeneratedProblem(filename=os.path.join(example_path,"AuxiliaryServices.csv")), PointSearch)
     print("Nadir: %s"%method.problem.nadir)
     print("Ideal: %s"%method.problem.ideal)
+    tui.iter_enautilus(method)
+    solution=method.zh_prev  
     
-    if tui:
-        method.user_iters=method.current_iter=int(prompt(u'Ni: ',default=u"5",validator=NumberValidator()))
-        method.Ns=int(prompt(u'Ns: ',default=u"5",validator=NumberValidator()))
-        method.nextIteration()
-    else:
-        points=None
-    
-    while method.current_iter:
-        if tui:
-            pref=select_iter(method,1)
-            if pref is None:
-                break
-            
-            points=method.nextIteration(pref)
-        else:
-            points=method.nextIteration(points)
-            for ri,r in enumerate(method.zh_reach):
-                if r:
-                    points=points[ri]
-                    break
-                            
-            method.printCurrentIteration()
+    if method.current_iter:
+        if solution is None:
+            solution = method.problem.nadir
+        method = NAUTILUSv1(PreGeneratedProblem(filename=os.path.join(example_path,"AuxiliaryServices.csv")), PointSearch)
+        solution = tui.iter_nautilus(method)
 
-    if method.current_iter:          
-        method_v1 = NAUTILUSv1(PreGeneratedProblem(os.path.join(example_path,"AuxiliaryServices.csv")), PointSearch)
-        method_v1.zh=method.zh_prev
-        method_v1.zh_prev=method.zh_prev
-        method_v1.current_iter=method.current_iter
-        method_v1.user_iters=method.user_iters
-        method_v1.printCurrentIteration()
-        pref=RelativeRanking([2, 2, 1])
-        
-        while method_v1.current_iter:
-            if tui:
-                rank=prompt(u'Ranking: ',default=u",".join(map(str,pref.pref_input)),validator=VectorValidator(method))
-                if rank=="c":
-                    break
-                pref=RelativeRanking(map(float,rank.split(",")))
-    
-            solution, distance = method_v1.nextIteration(pref)
-            method_v1.printCurrentIteration()
-    else:
-        method.printCurrentIteration()
-
-    
-    #printCurrentIteration(method)
-
-    # solution, distance = method.nextIteration(DirectSpecification([-5, -3, -3, 5]))
-    #solution, distance = method.nextIteration(RelativeRanking([2, 2, 1]))
-    #printCurrentIteration(method)
-
-    #while (method.current_iter > 0):
-        #solution, distance = method.nextIteration()
-        #printCurrentIteration(method)
-
-    # print transfer_point
-
-    # For Narula-Weistroffoer generate set of alternatives with e.g. Steurs method
-    # By changing weight vectors for the ref_point
-    # Getting PO set for problem, how to generate the weights Interactive WASGFA
-
-
-    # check several pareto points here between the current bounds
-    # e_nautilus = ENAUTILUS(NaurulaWeistroffer(), OptimalSearch, starting_point=transfer_point)
-    # print e_nautilus.iterationPoint(DirectSpecification([-5, -3, -3, 5]))
+    method.printCurrentIteration()
 
     try:
         from prompt_toolkit import prompt
