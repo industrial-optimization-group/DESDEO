4689451efc72d4c48874be85459313d42b6f4e90
Vesa Ojalehto <ferrety@users.noreply.github.com>
2017-07-03 21:16:59 +0300

    Let's pretend we have tests

---
+++ tests/test_NNAUTILUS.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+
+from pyDESDEO.method.NAUTILUS import NNAUTILUS
+from pyDESDEO.examples.NarulaWeistroffer import NaurulaWeistroffer, WEIGHTS
+from pyDESDEO.problem.Problem import PreGeneratedProblem
+from pyDESDEO.optimization.OptimizationMethod import PointSearch
+from pyDESDEO.examples.AuxiliaryServices import example_path
+
+
+def run(method, ref_point, steps = 100, bounds = None):
+    ''' test method for steps iterations with given  reference point and bounds (if any)'''
+    vals = []
+    for i in range(steps):
+        dist, fh, zh, lo, up, nP = method.nextIteration(ref_point, bounds)
+        vals.append([dist, lo, up, zh])
+        if not nP:
+            break
+    return vals
+
+
+def test_running():
+    method = NNAUTILUS(PreGeneratedProblem(filename=os.path.join(example_path,"AuxiliaryServices.csv")), PointSearch)
+    vals = run(method, [-30000, 4, -60], 6)
+    assert len(vals) == 6
+
+    
