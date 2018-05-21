767a84dc7076e7d94b85af865d84a05c7f8f7600
Frankie Robertson <frankie@robertson.name>
2018-05-21 12:06:31 +0300

    Fix classifiers in setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)"
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: PyPy",
