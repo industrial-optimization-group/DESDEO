f60514020a2f7331094e6d84647dde260df86ef3
Frankie Robertson <frankie@robertson.name>
2018-05-21 12:28:28 +0300

    Version bump => 0.1.1

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # History #
 
+## 0.1.1 (2018-05-21) ##
+
+* Convert package description to use Markdown
+
 ## 0.1.0 (2018-04-25) ##
 
 * First release
--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     description="Open source library for for interactive multiobjective optimization",
     long_description=long_desc,
     long_description_content_type="text/markdown",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["desdeo"]),
     package_dir={"desdeo": "desdeo"},
     install_requires=requirements,
