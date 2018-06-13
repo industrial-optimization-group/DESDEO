bb8c7f236346d09f406a9e377577b70bdce3e9d1
Frankie Robertson <frankie@robertson.name>
2018-06-13 14:09:12 +0300

    Fix importing accident

--- desdeo/result/__init__.py
+++ desdeo/result/__init__.py
@@ -3,6 +3,6 @@ This package contains classes for representing results obtained from running
 the methods in `desdeo.method`.
 """
 
-from Result import ResultSet
+from .Result import ResultSet
 
 __all__ = ["ResultSet"]
