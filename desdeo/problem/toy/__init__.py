7c23d85040a439e4f42c03a9455872b6c3e1e87d
Frankie Robertson <frankie@robertson.name>
2018-07-16 15:59:49 +0300

    Fix import ordering

--- desdeo/problem/toy/__init__.py
+++ desdeo/problem/toy/__init__.py
@@ -3,7 +3,7 @@ This module contains simple "toy" problems suitable for demonstrating different
 interactive multi-objective optimization methods.
 """
 
-from .river_pollution import RiverPollution
 from .cylinder import CylinderProblem
+from .river_pollution import RiverPollution
 
 __all__ = ["RiverPollution", "CylinderProblem"]
