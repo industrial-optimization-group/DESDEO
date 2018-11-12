0a7bb9bcbc6de6fde1ae2ca5f53d2293de0da53d
Babooshka <43251215+bshavazipour@users.noreply.github.com>
2018-11-12 12:07:38 +0200

    Delete SurrogatePrediction.m

--- ANOVA-MOP/Metamodeling/SurrogatePrediction.m
+++
@@ -1,8 +0,0 @@
-function Pred=SurrogatePrediction(x0,model)
-md=model.md;
-check3=model.check3;
-P=model.P;
-MaxIntOrder=model.MaxIntOrder;
-x0=MultivariateLegendre2(x0,P,MaxIntOrder);
-x0=x0(:,check3);
-Pred=x0*md;
