7ef154e32b7148655777b189147804f032a57c5f
Babooshka <43251215+bshavazipour@users.noreply.github.com>
2018-11-12 12:08:44 +0200

    Delete MyFun.m

--- ANOVA-MOP/CommonFiles/MyFun.m
+++
@@ -1,7 +0,0 @@
-function Value=MyFun(X0,lb,ub,ProbName,ObjInd,ObjNum)
-range=ub-lb;
-n=size(X0,1);
-X=((X0+1).*repmat(range,n,1))/2+repmat(lb,n,1);
-fTemp=P_objective('value',ProbName,ObjNum,X);
-Value=fTemp(:,ObjInd);
-end
