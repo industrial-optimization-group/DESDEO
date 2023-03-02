42707326b607d8a64dfe8b8e179caf3246fac7f0
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2023-03-02 13:42:48 +0200

    Updated deps, bumbed version number, moved to Python 3.9 and 3.10, updated requirements.txt, added readthedocs conf. Fixed docs.

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Multiobjective Optimization Group"
 author = "Multiobjective Optimization Group"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.2"
+release = "1.2.0"
 
 
 # -- General configuration ---------------------------------------------------
