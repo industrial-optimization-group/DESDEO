b1253d38015bfb032603029713ef66c0948e3fd3
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-01-10 16:47:17 +0200

    Work on the problem schema.

--- desdeo/problem/schema.py
+++ desdeo/problem/schema.py
@@ -20,13 +20,16 @@ MAX_SHORTNAME_LENGTH = 3
 class Constant(BaseModel):
     """Model for a constant."""
 
-    longname: str = Field(
-        description="Long descriptive name of the constant. This can be used in UI and visualizations.",
-    )
-    shortname: str = Field(
+    name: str = Field(
         description=(
-            "Short name of the constant. This will be used in the rest of the problem definition."
-            " It may also be used in UIs and visualizations."
+            "Descriptive name of the constant. This can be used in UI and visualizations.",
+            " Example: 'maximum cost'.",
+        ),
+    )
+    symbol: str = Field(
+        description=(
+            "Symbol to represent the constant. This will be used in the rest of the problem definition."
+            " It may also be used in UIs and visualizations. Example: 'c_1'."
         ),
         max_length=MAX_SHORTNAME_LENGTH,
     )
@@ -44,13 +47,13 @@ class VariableTypes(Enum):
 class Variable(BaseModel):
     """Model for a variable."""
 
-    longname: str = Field(
-        description="Long descriptive name of the variable. This can be used in UI and visualizations.",
+    name: str = Field(
+        description="Descriptive name of the variable. This can be used in UI and visualizations. Example: 'velocity'."
     )
-    shortname: str = Field(
+    symbol: str = Field(
         description=(
-            "Short name of the variable. This will be used in the rest of the problem definition."
-            " It may also be used in UIs and visualizations."
+            "Symbol to represent the variable. This will be used in the rest of the problem definition."
+            " It may also be used in UIs and visualizations. Example: 'v_1'."
         ),
         max_length=MAX_SHORTNAME_LENGTH,
     )
@@ -62,53 +65,65 @@ class Variable(BaseModel):
     )
 
 
-class ExtraFuncs(BaseModel):
+class ExtraFunction(BaseModel):
     """Model for extra functions."""
 
     ...
 
 
-class Objective(BaseModel):
-    """Model for an objective."""
+class ScalarizationFunction(BaseModel):
+    """Model for scalarization of the problem."""
 
-    longname: str = Field(
-        description="Long descriptive name of the objective. This can be used in UI and visualizations.",
-    )
-    shortname: str = Field(
+    ...
+
+
+class Objective(BaseModel):
+    """Model for an objective function."""
+
+    name: str = Field(
         description=(
-            "Short name of the objective. This will be used in the rest of the problem definition."
-            " It may also be used in UIs and visualizations."
+            "Descriptive name of the objective function. This can be used in UI and visualizations.",
+            " Example: 'time'.",
+        ),
+    )
+    symbol: str = Field(
+        description=(
+            "Symbol to represent the objective function. This will be used in the rest of the problem definition."
+            " It may also be used in UIs and visualizations. Example: 'f_1'."
         ),
         max_length=MAX_SHORTNAME_LENGTH,
     )
     func: list = Field(
         description=(
-            "Function of the objective. This is a JSON object that can be parsed into a function."
+            "The objective function. This is a JSON object that can be parsed into a function."
             "Must be a valid MathJSON object. The symbols in the function must match variable/constant shortnames."
         ),
     )
     maximize: bool = Field(
-        description="Whether the objective is to be maximized or minimized.",
+        description="Whether the objective function is to be maximized or minimized.",
         default=False,
     )
     ideal: float | None = Field(
         description="Ideal value of the objective. This is optional.",
-    )  # Note: The examples use the term "lowerbound" instead of "ideal". That is a mistake.
+    )
     nadir: float | None = Field(
         description="Nadir value of the objective. This is optional.",
-    )  # Note: The examples use the term "upperbound" instead of "nadir". That is a mistake.
+    )
 
 
 class Constraint(BaseModel):
-    """Model for a constraint."""
+    """Model for a constraint function."""
 
-    longname: str = Field(
-        description="Long descriptive name of the constraint. This can be used in UI and visualizations.",
+    name: str = Field(
+        description=(
+            "Descriptive name of the constraint. This can be used in UI and visualizations."
+            " Example: 'maximum length'"
+        ),
     )
     shortname: str = Field(
         description=(
-            "Short name of the constraint. This will be used in the rest of the problem definition."
-            " It may also be used in UIs and visualizations."
+            "Symbol to represent the constraint. This will be used in the rest of the problem definition."
+            " It may also be used in UIs and visualizations. Example: 'g_1'."
         ),
         max_length=MAX_SHORTNAME_LENGTH,
     )
@@ -119,6 +134,12 @@ class Constraint(BaseModel):
             " The symbols in the function must match objective/variable/constant shortnames."
         ),
     )
+
+
+class EvaluatedPoint(BaseModel):
+    """Model to represent the evaluated objective values of an decision vector."""
+
+    ...
 
 
 class Problem(BaseModel):
@@ -142,13 +163,19 @@ class Problem(BaseModel):
     constraints: list[Constraint] | None = Field(
         description="Optional list of constraints.",
     )
-    extra_funcs: list[ExtraFuncs] | None = Field(
+    extra_funcs: list[ExtraFunction] | None = Field(
         description="Optional list of extra functions. Use this if some function is repeated multiple times.",
+    )
+    scalarizations_funcs: list[ScalarizationFunction] | None = Field(
+        description="Optional list of scalarization functions representing the problem."
     )
 
 
+"""
 class DataObjective(Objective):
-    """Model for a data-based objective definition."""
+"""
+"""Model for a data-based objective definition."""
+"""
 
     func: list | None = Field(
         description=(
@@ -156,10 +183,13 @@ class DataObjective(Objective):
             "Must be a valid MathJSON object. The symbols in the function must match variable/constant shortnames."
         ),
     )
+"""
 
-
+"""
 class DataProblem(BaseModel):
-    """Model for a data-based problem definition."""
+"""
+"""Model for a data-based problem definition."""
+"""
 
     name: str = Field(
         description="Name of the problem.",
@@ -185,3 +215,4 @@ class DataProblem(BaseModel):
             " Each column is a variable, objective, or constraint.",
         ),
     )
+"""
