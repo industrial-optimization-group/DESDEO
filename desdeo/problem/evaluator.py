08f4c648cab55dfea4f68d5bce92cd97e8a7133b
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-01-16 16:47:31 +0200

    Work on the mock implementation of the Evaluator class.

--- desdeo/problem/evaluator.py
+++ desdeo/problem/evaluator.py
@@ -3,6 +3,44 @@
 from desdeo.problem.schema import Problem
 from desdeo.problem.parser import MathParser
 
+from pydantic import BaseModel, Field
+
+
+class EvaluatorResult(BaseModel):
+    """A model to store the results computed by Evaluator."""
+
+    variable_values: dict[str, list[float]] = Field(
+        description=(
+            "The decision variable values utilized in the evaluation. The keys of the dict are the decision variable"
+            " symbols followed by a list of values corresponding to the variable."
+        )
+    )
+    objective_values: dict[str, list[float]] = Field(
+        description=(
+            "The evaluated objective function values. The keys of the dict are objective function symbols followed"
+            " by a list of values corresponding to the objective function."
+        )
+    )
+    extra_values: dict[str, list[float]] | None = Field(
+        description=(
+            "The evaluated extra function values. The keys of the dict are the extra function symbols followed"
+            " by a list of values corresponding to the extra function. Optional."
+        )
+    )
+    constraint_values: dict[str, list[float]] | None = Field(
+        description=(
+            "The evaluated constraint values. The keys of the dict"
+            "are the constraint symbols followed by a list of values corresponding to the constraint. Optional."
+        )
+    )
+    scalarization_values: dict[str, list[float]] | None = Field(
+        description=(
+            "The evaluated scalarization function values. The"
+            " keys of the dict are the names of the scalarization functions followed by a list corresponding to the"
+            " functions. Optional."
+        )
+    )
+
 
 class Evaluator:
     """A class for creating evaluators for multiobjective optimization problems."""
@@ -32,5 +70,26 @@ class Evaluator:
         Args:
             problem (Problem): The problem as a pydantic 'Problem' data class.
             evaluator_type (str): The type of evaluator. Default 'polars'.
+        """
+        # 1. Create a MathParser of type 'evaluator_type'.
+        ...
+        # 2. If any constants are defined in problem, replace their symbol with the defined numerical
+        #    value in all the function expressions found in the Problem.
+        ...
+        # 3. Parse said function expressions into a dataframe.
+        ...
+        # 4. The dataframe is stored in the class instance.
+        self.dataframe = None
+
+    def evaluate(self, xs: dict[str, list[float | int | bool]]) -> EvaluatorResult:
+        """Evaluate the problem with the given decision variable values.
+
+        Args:
+            xs (dict[str, list[float  |  int  |  bool]]): a dict with the decision variable names
+            as the keys followed by the corresponding decision variable values stored in a list.
+            Each list in the dict should contain the same number of values.
+
+        Returns:
+            EvaluatorResult: the results of the evaluation. See `EvaluatorResult`.
         """
         ...
