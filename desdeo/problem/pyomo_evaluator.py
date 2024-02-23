3aa8f4e8b78a13869bdad42876b06b4aa8392ff7
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-02-23 17:35:17 +0200

    Progress on the pyomo evaluator.
    The initialization of the pyomo model in the PyomoEvaluator should now work. Next, implement the evaluate method.

--- desdeo/problem/pyomo_evaluator.py
+++ desdeo/problem/pyomo_evaluator.py
@@ -1,18 +1,10 @@
 """Defines an evaluator compatible with the Problem JSON format and transforms it into a Pyomo model."""
-from pyomo.environ import (
-    ConcreteModel,
-    Expression,
-    Var,
-    Integers,
-    NegativeIntegers,
-    NonNegativeIntegers,
-    Reals,
-    NegativeReals,
-    NonNegativeReals,
-    Param,
-)
+from operator import le as _le
+from operator import eq as _eq
 
-from desdeo.problem import Problem, VariableTypeEnum, FormatEnum, MathParser
+import pyomo.environ as pyomo
+
+from desdeo.problem import ConstraintTypeEnum, Problem, VariableTypeEnum, FormatEnum, MathParser
 
 
 class PyomoEvaluatorError(Exception):
@@ -24,42 +16,60 @@ class PyomoEvaluator:
 
     def __init__(self, problem: Problem):
         """Summary."""
-        model = ConcreteModel()
+        model = pyomo.ConcreteModel()
 
         # set the parser
         self.parse = MathParser(to_format=FormatEnum.pyomo).parse
+
         # Add variables
         model = self.init_variables(problem, model)
+
         # Add constants
         model = self.init_constants(problem, model)
-        # Add extra expressions
+
+        # Add extra expressions, if any
         if problem.extra_funcs is not None:
             model = self.init_extras(problem, model)
 
+        # Add objective function expressions
+        model = self.init_objectives(problem, model)
+
+        # Add constraints, if any
+        if problem.constraints is not None:
+            model = self.init_constraints(problem, model)
+
+        # Add scalarization functions, if any
+        if problem.scalarizations_funcs is not None:
+            model = self.init_scalarizations(problem, model)
+
         self.model = model
 
-    def init_variables(self, problem, model):
+    def init_variables(self, problem: Problem, model: pyomo.Model) -> pyomo.Model:
         """Add variables to the pyomo model.
 
         Args:
             problem (Problem): problem from which to extract the variables.
+            model (pyomo.Model): the pyomo model to add the variables to.
 
         Raises:
             PyomoEvaluator: when a problem in extracting the variables is encountered.
                 I.e., the bounds of the variables are incorrect or of a non supported type.
+
+        Returns:
+            pyomo.Model: the pyomo model with the variables added as attributes.
         """
         for var in problem.variables:
             # figure out the variable type
             match (var.lowerbound >= 0, var.upperbound >= 0, var.variable_type):
                 case (True, True, VariableTypeEnum.integer):
                     # variable is positive integer
-                    domain = NonNegativeIntegers
+                    domain = pyomo.NonNegativeIntegers
                 case (False, False, VariableTypeEnum.integer):
                     # variable is negative integer
-                    domain = NegativeIntegers
+                    domain = pyomo.NegativeIntegers
                 case (False, True, VariableTypeEnum.integer):
                     # variable can be both negative an positive integer
-                    domain = Integers
+                    domain = pyomo.Integers
                 case (True, False, VariableTypeEnum.integer):
                     # error! lower bound is greater than upper bound
                     msg = (
@@ -69,13 +79,13 @@ class PyomoEvaluator:
                     raise PyomoEvaluatorError(msg)
                 case (True, True, VariableTypeEnum.real):
                     # variable is positive real
-                    domain = NonNegativeReals
+                    domain = pyomo.NonNegativeReals
                 case (False, False, VariableTypeEnum.real):
                     # variable is negative real
-                    domain = NegativeReals
+                    domain = pyomo.NegativeReals
                 case (False, True):
                     # variable can be both negative and positive real
-                    domain = Reals
+                    domain = pyomo.Reals
                 case (True, False):
                     # eror! lower bound is greater than upper bound
                     msg = (
@@ -90,56 +100,152 @@ class PyomoEvaluator:
             setattr(
                 model,
                 var.symbol,
-                Var(
+                pyomo.Var(
                     name=var.name, bounds=(var.lowerbound, var.upperbound), initialize=var.initial_value, domain=domain
                 ),
             )
 
         return model
 
-    def init_constants(self, problem: Problem, model):
-        """Add constants to the pyomo model.
+    def init_constants(self, problem: Problem, model: pyomo.Model) -> pyomo.Model:
+        """Add constants to a pyomo model.
 
         Args:
             problem (Problem): problem from which to extract the constants.
+            model (pyomo.Model): the pyomo model to add the constants to.
 
         Raises:
             PyomoEvaluatorError: when the domain of a constant cannot be figure out.
+
+        Returns:
+            pyomo.Model: the pyomo model with the constants added as attributes.
         """
         for con in problem.constants:
             # figure out the domain of the constant
             match (isinstance(con.value, int), isinstance(con.value, float), con.value >= 0):
                 case (True, False, True):
                     # positive integer
-                    domain = NonNegativeIntegers
+                    domain = pyomo.NonNegativeIntegers
                 case (True, False, False):
                     # negative integer
-                    domain = NegativeIntegers
+                    domain = pyomo.NegativeIntegers
                 case (False, True, True):
                     # positive real
-                    domain = NonNegativeReals
+                    domain = pyomo.NonNegativeReals
                 case (False, True, False):
                     # negative real
-                    domain = NegativeReals
+                    domain = pyomo.NegativeReals
                 case _:
                     # not possible, something went wrong
                     msg = f"Failed to figure out the domain for the constant {con.symbol}."
                     raise PyomoEvaluatorError(msg)
 
-            setattr(model, con.symbol, Param(name=con.name, initialize=con.value, domain=domain))
+            setattr(model, con.symbol, pyomo.Param(name=con.name, initialize=con.value, domain=domain))
 
         return model
 
-    def init_extras(self, problem: Problem, model):
-        """Add extra function expressions to the pyomo model.
+    def init_extras(self, problem: Problem, model: pyomo.Model):
+        """Add extra function expressions to a pyomo model.
 
         Args:
             problem (Problem): problem from which the extract the extra function expressions.
+            model (pyomo.Model): the pyomo model to add the extra function expressions to.
+
+        Returns:
+            pyomo.Model: the pyomo model with the expressions added as attributes.
         """
         for extra in problem.extra_funcs:
-            # to be done
             pyomo_expr = self.parse(extra.func, model)
 
             setattr(model, extra.symbol, pyomo_expr)
+
+        return model
+
+    def init_objectives(self, problem: Problem, model: pyomo.Model) -> pyomo.Model:
+        """Add objective function expressions to a pyomo model.
+
+        Args:
+            problem (Problem): problem from which to extract the objective function expresions.
+            model (pyomo.Model): the pyomo model to add the expressions to.
+
+        Returns:
+            pyomo.Model: the pyomo model with the objective expressions added as pyomo Objectives.
+                The objectives are deactivated by default.
+        """
+        for obj in problem.objectives:
+            pyomo_expr = self.parse(obj.func, model)
+
+            obj_expr = pyomo.Objective(
+                expr=pyomo_expr,
+                sense=pyomo.maximize if obj.maximize else pyomo.minimize,
+                name=obj.name,
+            )
+            obj_expr.deactivate()
+
+            setattr(model, obj.symbol, obj_expr)
+
+            obj_min_expr = pyomo.Objective(
+                expr=-obj_expr if obj.maximize else obj_expr,
+                sense=pyomo.minimize,
+                name=obj.name,
+            )
+            obj_min_expr.deactivate()
+
+            # the obj.symbol_min objectives are used when optimizing and building scalarizations etc...
+            setattr(model, f"{obj.symbol}_min", obj_min_expr)
+
+        return model
+
+    def init_constraints(self, problem: Problem, model: pyomo.Model) -> pyomo.Model:
+        """Add constraint expressions to a pyomo model.
+
+        Args:
+            problem (Problem): the problem from which to extract the constraint function expressions.
+            model (pyomo.Model): the pyomo model to add the exprssions to.
+
+        Raises:
+            PyomoEvaluatorError: when an unsupported constraint type is encountered.
+
+        Returns:
+            pyomo.Model: the pyomo model with the constraint expressions added as pyomo Constraints.
+        """
+        for cons in problem.constraints:
+            pyomo_expr = self.parse(cons.func, model)
+
+            match con_type := cons.cons_type:
+                case ConstraintTypeEnum.LTE:
+                    # constraints in DESDEO are defined such that they must be less than zero
+                    pyomo_expr = _le(pyomo_expr, 0)
+                case ConstraintTypeEnum.EQ:
+                    pyomo_expr = _eq(pyomo_expr, 0)
+                case _:
+                    msg = f"Constraint type of {con_type} not supported. Must be one of {ConstraintTypeEnum}."
+                    raise PyomoEvaluatorError(msg)
+
+            cons_expr = pyomo.Constraint(expr=pyomo_expr, name=cons.name)
+
+            setattr(model, cons.symbol, cons_expr)
+
+        return model
+
+    def init_scalarizations(self, problem: Problem, model: pyomo.Model) -> pyomo.Model:
+        """Add scalrization expressions to a pyomo model.
+
+        Args:
+            problem (Problem): the problem from which to extract thescalarization function expressions.
+            model (pyomo.Model): the pyomo model to add the expressions to.
+
+        Returns:
+            pyomo.Model: the pyomo model with the scalarization expressions addedd as pyomo Objectives.
+                The objectives are deactivated by default. Scalarization functions are always minimized.
+        """
+        for scal in problem.scalarizations_funcs:
+            pyomo_expr = self.parse(scal.func, model)
+
+            # scalarization functions are always assumed to be minimized!
+            scal_expr = pyomo.Objective(expr=pyomo_expr, name=scal.name, sense=pyomo.minimize)
+            scal_expr.deactivate()
+
+            setattr(model, scal.symbol, scal_expr)
 
         return model
--- tests/test_pyomo_evaluator.py
+++ tests/test_pyomo_evaluator.py
@@ -1,26 +1,26 @@
 """Tests for testing the PyomoEvaluator."""
 from desdeo.problem.pyomo_evaluator import PyomoEvaluator
-from desdeo.problem import binh_and_korn, ExtraFunction
-from pyomo.environ import Objective
+from desdeo.problem import binh_and_korn, ExtraFunction, ScalarizationFunction
+
+import pyomo.environ as pyomo
 import pytest
 
 
 @pytest.fixture
 def binh_and_korn_w_extra():
-    """Defines a variant of the Bing and Korn function with extra functions."""
+    """Defines a variant of the Bing and Korn function with extra functions and scalarizations."""
     problem = binh_and_korn()
     extra_1 = ExtraFunction(name="Extra 1", symbol="extr_1", func="x_1 + x_2")
     extra_2 = ExtraFunction(name="Extra 2", symbol="extr_2", func="x_2 - x_1")
 
-    return problem.model_copy(
-        update={
-            "extra_funcs": [extra_1, extra_2],
-        }
-    )
+    scal_1 = ScalarizationFunction(name="scal 1", symbol="s_1", func="x_1 + 2.4*x_2 - x_1")
+    scal_2 = ScalarizationFunction(name="scal 2", symbol="s_2", func="x_2 + x_2 - 3*x_1")
+
+    return problem.model_copy(update={"extra_funcs": [extra_1, extra_2], "scalarizations_funcs": [scal_1, scal_2]})
 
 
 def test_w_binh_and_korn(binh_and_korn_w_extra):
-    """Tests the evalutor with the Binh and Korn problem."""
+    """Tests the initialization of the evalutor with the Binh and Korn problem."""
     problem = binh_and_korn_w_extra
     evaluator = PyomoEvaluator(problem)
 
@@ -36,5 +36,26 @@ def test_w_binh_and_korn(binh_and_korn_w_extra):
     for extra in problem.extra_funcs:
         assert hasattr(evaluator.model, extra.symbol)
 
-    assert getattr(evaluator.model, problem.extra_funcs[0].symbol).to_string() == "x_1 + x_2"
-    assert getattr(evaluator.model, problem.extra_funcs[1].symbol).to_string() == "x_2 - x_1"
+    # has all objectives, including the _min ones.
+    for obj in problem.objectives:
+        assert hasattr(evaluator.model, obj.symbol)
+        assert hasattr(evaluator.model, f"{obj.symbol}_min")
+
+        # all objectives should be deactivated by default
+        assert not getattr(evaluator.model, obj.symbol).active
+        assert not getattr(evaluator.model, f"{obj.symbol}_min").active
+
+        assert isinstance(getattr(evaluator.model, obj.symbol), pyomo.Objective)
+
+    # has all constraints
+    for cons in problem.constraints:
+        assert hasattr(evaluator.model, cons.symbol)
+        assert isinstance(getattr(evaluator.model, cons.symbol), pyomo.Constraint)
+
+    # has all scalarizations
+    for scal in problem.scalarizations_funcs:
+        assert hasattr(evaluator.model, scal.symbol)
+        assert isinstance(getattr(evaluator.model, scal.symbol), pyomo.Objective)
+
+        # scalarizations should be deactivated by default
+        assert not getattr(evaluator.model, scal.symbol).active
