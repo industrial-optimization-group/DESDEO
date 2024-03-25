194f8b9af9ea4bec53e0363625ebcece52f36cfb
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-03-25 17:03:35 +0200

    Documentation changes.
    Fixed iffy LaTeX rendering when jumping from section to section using
    the TOC on the right. Should now render LaTeX without a need to
    constantly refresh.

--- docs/extrajs/extra.js
+++
@@ -1,9 +0,0 @@
-<script>
-    window.MathJax = {
-        tex: {
-            tags: 'ams' // Enables automatic equation numbering
-        }
-    };
-</script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
-
