736645829caec6d2412102f5a60e0da5379cadec
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-04-04 15:44:08 +0300

    Sympy evaluator done and tested.
    Seems to work fine for now.

--- desdeo/problem/sympy_evaluator.py
+++ desdeo/problem/sympy_evaluator.py
@@ -1,5 +1,7 @@
 """Implements and evaluator based on sympy expressions."""
 
+from copy import deepcopy
+
 from desdeo.problem.json_parser import MathParser, FormatEnum
 from desdeo.problem.schema import Problem
 
@@ -19,29 +21,191 @@ class SympyEvaluator:
         Args:
             problem (Problem): the problem to be evaluated.
         """
+        # Collect all the symbols and expressions in the problem
+        parser = MathParser(to_format=FormatEnum.sympy)
+
         self.variable_symbols = [var.symbol for var in problem.variables]
         self.constant_expressions = (
-            [(const.symbol, sp.sympify(const.value, evaluate=False)) for const in problem.constants]
+            {const.symbol: parser.parse(const.value) for const in problem.constants}
             if problem.constants is not None
             else None
         )
 
         self.extra_expressions = (
-            [(extra.symbol, sp.sympify(extra.func, evaluate=False)) for extra in problem.extra_funcs]
+            {extra.symbol: parser.parse(extra.func) for extra in problem.extra_funcs}
             if problem.extra_funcs is not None
             else None
         )
 
-        self.objective_expressions = [(obj.symbol, sp.sympify(obj.func, evaluate=False)) for obj in problem.objectives]
+        self.objective_expressions = {obj.symbol: parser.parse(obj.func) for obj in problem.objectives}
 
         self.constraint_expressions = (
-            [(con.symbol, sp.sympify(con.func, evaluate=False)) for con in problem.constraints]
+            {con.symbol: parser.parse(con.func) for con in problem.constraints}
             if problem.constraints is not None
             else None
         )
 
         self.scalarization_expressions = (
-            [(scal.symbol, sp.sympify(scal.func, evaluate=False)) for scal in problem.scalarization_funcs]
+            {scal.symbol: parser.parse(scal.func) for scal in problem.scalarization_funcs}
             if problem.scalarization_funcs is not None
             else None
         )
+
+        # replace symbols and create lambda functions ready to be called
+        # replace constants in extra functions, if they exist
+        if self.extra_expressions is not None:
+            _extra_expressions = (
+                {
+                    k: self.extra_expressions[k].subs(self.constant_expressions, evaluate=False)
+                    for k in self.extra_expressions
+                }
+                if self.constant_expressions is not None
+                else deepcopy(self.extra_expressions)
+            )
+        else:
+            _extra_expressions = None
+
+        # replace constants in objective functions, if constants have been defined
+        _objective_expressions = (
+            {
+                k: self.objective_expressions[k].subs(self.constant_expressions, evaluate=False)
+                for k in self.objective_expressions
+            }
+            if self.constant_expressions is not None
+            else deepcopy(self.objective_expressions)
+        )
+
+        # replace extra functions in objective functions, if extra functions have been defined
+        _objective_expressions = (
+            (
+                {
+                    k: _objective_expressions[k].subs(self.extra_expressions, evaluate=False)
+                    for k in _objective_expressions
+                }
+            )
+            if self.extra_expressions is not None
+            else _objective_expressions
+        )
+
+        # always minimized objective expressions
+        _objective_expressions_min = {
+            f"{obj.symbol}_min": -_objective_expressions[obj.symbol]
+            if obj.maximize
+            else _objective_expressions[obj.symbol]
+            for obj in problem.objectives
+        }
+
+        # replace stuff in the constraint expressions if any are defined
+        if self.constraint_expressions is not None:
+            # replace constants
+            _constraint_expressions = (
+                {
+                    k: self.constraint_expressions[k].subs(self.constant_expressions, evaluate=False)
+                    for k in self.constraint_expressions
+                }
+                if self.constant_expressions is not None
+                else deepcopy(self.constraint_expressions)
+            )
+
+            # replace extra functions
+            _constraint_expressions = (
+                {
+                    k: _constraint_expressions[k].subs(_extra_expressions, evaluate=False)
+                    for k in _constraint_expressions
+                }
+                if _extra_expressions is not None
+                else _constraint_expressions
+            )
+
+            # replace objective functions
+            _constraint_expressions = {
+                k: _constraint_expressions[k].subs(_objective_expressions, evaluate=False)
+                for k in _constraint_expressions
+            }
+            _constraint_expressions = {
+                k: _constraint_expressions[k].subs(_objective_expressions_min, evaluate=False)
+                for k in _constraint_expressions
+            }
+
+        else:
+            _constraint_expressions = None
+
+        # replace stuff in scalarization expressions if any are defined
+        if self.scalarization_expressions is not None:
+            # replace constants
+            _scalarization_expressions = (
+                {
+                    k: self.scalarization_expressions[k].subs(self.constant_expressions, evaluate=False)
+                    for k in self.scalarization_expressions
+                }
+                if self.constant_expressions is not None
+                else deepcopy(self.scalarization_expressions)
+            )
+
+            # replace extra functions
+            _scalarization_expressions = (
+                {
+                    k: _scalarization_expressions[k].subs(_extra_expressions, evaluate=False)
+                    for k in _scalarization_expressions
+                }
+                if _extra_expressions is not None
+                else _scalarization_expressions
+            )
+
+            # replace constraints
+            _scalarization_expressions = (
+                {
+                    k: _scalarization_expressions[k].subs(_constraint_expressions, evaluate=False)
+                    for k in _scalarization_expressions
+                }
+                if _constraint_expressions is not None
+                else _scalarization_expressions
+            )
+
+            # replace objectives
+            _scalarization_expressions = {
+                k: _scalarization_expressions[k].subs(_objective_expressions, evaluate=False)
+                for k in _scalarization_expressions
+            }
+
+            _scalarization_expressions = {
+                k: _scalarization_expressions[k].subs(_objective_expressions_min, evaluate=False)
+                for k in _scalarization_expressions
+            }
+
+        else:
+            _scalarization_expressions = None
+
+        # initialize callable lambdas
+        self.lambda_exprs = {
+            _k: _v
+            for _d in [
+                {k: sp.lambdify(self.variable_symbols, d[k]) for k in d}
+                for d in [
+                    _extra_expressions,
+                    _objective_expressions,
+                    _objective_expressions_min,
+                    _constraint_expressions,
+                    _scalarization_expressions,
+                ]
+                if d is not None
+            ]
+            for _k, _v in _d.items()
+        }
+
+        self.problem = problem
+        self.parser = parser
+
+    def evaluate(self, xs: dict[str, float | int | bool]) -> dict[str, float | int | bool]:
+        """Evaluate the problem with a given decision variable dict.
+
+        Args:
+            xs (dict[str, float  |  int  |  bool]): a dict with keys representing decision variable
+                symbols and values with the decision variable value.
+
+        Returns:
+            dict[str, float | int | bool]: a dict with keys corresponding to each symbol
+                defined for the problem being evaluated and the corresponding expression's
+                value.
+        """
+        return {k: self.lambda_exprs[k](**xs) for k in self.lambda_exprs} | xs
--- tests/test_sympy_evaluator.py
+++ tests/test_sympy_evaluator.py
@@ -4,14 +4,15 @@ import pytest
 import sympy as sp
 import numpy.testing as npt
 
-from desdeo.problem import SympyEvaluator, binh_and_korn, zdt1
-from desdeo.tools import add_achievement_sf_diff
+from desdeo.problem import SympyEvaluator, binh_and_korn, MathParser, FormatEnum, zdt1, river_pollution_problem
+from desdeo.tools import add_achievement_sf_diff, add_weighted_sums
 
 
 @pytest.mark.sympy
 def test_initialization():
     """Tests the correct initialization of the evaluator."""
-    problem = binh_and_korn()
+    parser = MathParser(to_format=FormatEnum.sympy)
+    problem = binh_and_korn(maximize=(False, True))
     evaluator = SympyEvaluator(problem)
 
     # variables symbols
@@ -20,9 +21,9 @@ def test_initialization():
     # constants
     constant_symbols = [s.symbol for s in problem.constants]
     constant_values = [s.value for s in problem.constants]
-    for i, t in enumerate(evaluator.constant_expressions):
-        assert t[0] == constant_symbols[i]
-        npt.assert_almost_equal(t[1].evalf(), constant_values[i])
+    for i, t in enumerate(d := evaluator.constant_expressions):
+        assert t == constant_symbols[i]
+        npt.assert_almost_equal(d[t].evalf(), constant_values[i])
 
     # extra expressions
     assert evaluator.extra_expressions is None
@@ -30,16 +31,16 @@ def test_initialization():
     # objective functions
     objective_symbols = [obj.symbol for obj in problem.objectives]
     objective_exprs = [obj.func for obj in problem.objectives]
-    for i, t in enumerate(evaluator.objective_expressions):
-        assert t[0] == objective_symbols[i]
-        assert t[1] == sp.sympify(objective_exprs[i], evaluate=False)
+    for i, t in enumerate(d := evaluator.objective_expressions):
+        assert t == objective_symbols[i]
+        assert d[t] == parser.parse(objective_exprs[i])
 
     # constraints
     constraint_symbols = [con.symbol for con in problem.constraints]
     constraint_exprs = [con.func for con in problem.constraints]
-    for i, t in enumerate(evaluator.constraint_expressions):
-        assert t[0] == constraint_symbols[i]
-        assert t[1] == sp.sympify(constraint_exprs[i], evaluate=False)
+    for i, t in enumerate(d := evaluator.constraint_expressions):
+        assert t == constraint_symbols[i]
+        assert d[t] == parser.parse(constraint_exprs[i])
 
     # scalarization
     assert evaluator.scalarization_expressions is None
@@ -54,10 +55,8 @@ def test_initialization():
 
     evaluator_w_asf = SympyEvaluator(problem_w_asf)
 
-    assert evaluator_w_asf.scalarization_expressions[0][0] == target
-    assert evaluator_w_asf.scalarization_expressions[0][1] == sp.sympify(
-        problem_w_asf.scalarization_funcs[0].func, evaluate=False
-    )
+    assert next(iter(evaluator_w_asf.scalarization_expressions.keys())) == target
+    assert evaluator_w_asf.scalarization_expressions[target] == parser.parse(problem_w_asf.scalarization_funcs[0].func)
 
     # extra
     problem_w_extras = zdt1(number_of_variables=10)
@@ -66,6 +65,77 @@ def test_initialization():
 
     extra_symbols = [extra.symbol for extra in problem_w_extras.extra_funcs]
     extra_exprs = [extra.func for extra in problem_w_extras.extra_funcs]
-    for i, t in enumerate(evaluator_w_extras.extra_expressions):
-        assert t[0] == extra_symbols[i]
-        assert t[1] == sp.sympify(extra_exprs[i], evaluate=False)
+    for i, t in enumerate(d := evaluator_w_extras.extra_expressions):
+        assert t == extra_symbols[i]
+        assert d[t] == parser.parse(extra_exprs[i])
+
+
+@pytest.mark.sympy
+def test_evaluate():
+    """Test that the evaluator evaluates correctly."""
+    problem = river_pollution_problem()
+    evaluator = SympyEvaluator(problem)
+
+    xs = {"x_1": 0.5, "x_2": 0.6}
+
+    res = evaluator.evaluate(xs)
+
+    for s in problem.get_all_symbols():
+        assert s in res
+
+    f_1_res = -5.205
+    npt.assert_almost_equal(res["f_1"], f_1_res)
+    npt.assert_almost_equal(res["f_1_min"], f_1_res)
+
+    f_2_res = -2.92703406574689
+    npt.assert_almost_equal(res["f_2"], f_2_res)
+    npt.assert_almost_equal(res["f_2_min"], f_2_res)
+
+    f_3_res = 7.36476190476191
+    npt.assert_almost_equal(res["f_3"], f_3_res)
+    npt.assert_almost_equal(res["f_3_min"], -f_3_res)
+
+    f_4_res = -0.355068493150685
+    npt.assert_almost_equal(res["f_4"], f_4_res)
+    npt.assert_almost_equal(res["f_4_min"], -f_4_res)
+
+    f_5_res = 0.15
+    npt.assert_almost_equal(res["f_5"], f_5_res)
+    npt.assert_almost_equal(res["f_5_min"], f_5_res)
+
+    # scalarization
+    ws = {"f_1": 0.1, "f_2": 0.4, "f_3": 0.2, "f_4": 0.05, "f_5": 0.25}
+    problem_w_sum, target = add_weighted_sums(problem, "target", ws)
+    evaluator_sum = SympyEvaluator(problem_w_sum)
+
+    res = evaluator_sum.evaluate(xs)
+
+    for s in problem_w_sum.get_all_symbols():
+        assert s in res
+
+    npt.assert_almost_equal(
+        res[target],
+        ws["f_1"] * f_1_res + ws["f_2"] * f_2_res + ws["f_3"] * -f_3_res + ws["f_4"] * -f_4_res + ws["f_5"] * f_5_res,
+    )
+
+    # constants and constraints
+    problem = binh_and_korn(maximize=(True, False))
+    evaluator = SympyEvaluator(problem)
+    xs = {"x_1": 2.5, "x_2": 3.2}
+
+    res = evaluator.evaluate(xs)
+
+    # minus constants
+    for s in problem.get_all_symbols():
+        if s in ["c_1", "c_2"]:
+            continue
+        assert s in res
+
+    npt.assert_almost_equal(res["f_1"], -65.96)
+    npt.assert_almost_equal(res["f_1_min"], 65.96)
+
+    npt.assert_almost_equal(res["f_2"], 9.49)
+    npt.assert_almost_equal(res["f_2_min"], 9.49)
+
+    npt.assert_almost_equal(res["g_1"], -8.51)
+    npt.assert_almost_equal(res["g_2"], -60.99)
