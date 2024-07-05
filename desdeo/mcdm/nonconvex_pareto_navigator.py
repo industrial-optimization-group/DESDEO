8c6fa3947d33c70effc20f48ebdd85a6cf6494ed
nieminme <mnieminen98@gmail.com>
2024-07-05 12:09:33 +0300

    Added comments on the current state of the implementation of Nonconvex pareto Navigator.

--- desdeo/mcdm/nonconvex_pareto_navigator.py
+++ desdeo/mcdm/nonconvex_pareto_navigator.py
@@ -6,6 +6,38 @@ Hartikainen, M., Miettinen, K., & Klamroth, K. (2019). Interactive nonconvex par
 for multiobjective optimization. European Journal of Operational Research, 275(1), 238-251.
 """
 
+"""NOTE: Work in progress, some issues listed below.
+
+The method works with the PAINT approximation for convex problems. Forming the e-cones is something that
+need to be done more properly than what is done here with better knowledge on how it was meant to be implemented.
+
+Some ISSUES at the moment:
+    - How is the epsilon value to be computed without a set of approximated solutions as PAINT approximation
+    does not provide this but a surrogate problem instead?
+        - SUGGESTION: Use some method to compute a representative set of approximated solutions using the surrogate
+        problem provided by the PAINT approximation. The surrogate problem has been implemented here as create_milp,
+        which can be used with PAINT by not providing the v_matrix and solutions array. These solutions could then
+        be used to evaluate the epsilon value.
+        - SUGGESTION: We assume a representative set of precomputed Pareto optimal solutions (same set as used in
+        the PAINT approximation perhaps) and use these to evaluate the epsilon as described in the article.
+
+    - With the epsilon value, the V matrix can be formed and the used to create the e-cones (constraints for the milp).
+    How are the e-cones (the V matrix) meant to be implemented into the problem? As in what are the z-vector and
+    0-vector in the article (Vz >= 0)?
+        - SUGGESTION: The z is the objective function values we are trying to compute (f_1, f_2, etc...) and
+        the 0-vector is a solution (an appoximated or Pareto optimal as described above), and these e-cones
+        are then formed as constraints for all of the solutions in the set.
+
+    - When the e-cones can be formed, should the PAINT provided surrogate problem be then modified and the constraints
+    be REPLACED by the constraints provided by the V matrix in e-cones (Vz >= 0 in the article)? As otherwise (I think),
+    the e-cone constraints would not have an effect.
+        - At the moment, a new mixed-integer linear problem is formed when the e-cones are available, using create_milp.
+        If the constraints from the surrogate problem are to be replaced, the problem should probably be reformed
+        (unless you can remove all the constraints coming from the PAINT surrogate problem). If the constraints do not
+        need to be replaced, the constraints from the e-cones can simply be added to the surrogate problem in some
+        separate function so the create_milp function is not called again.
+"""
+
 import numpy as np
 
 from desdeo.problem import (
@@ -86,7 +118,7 @@ def get_paint_approximation(problem: Problem, paint_approx: np.ndarray | None = 
         matrix.append(row)
     return np.array(matrix)
 
-def e_cones(problem: Problem, solutions: list[dict[str, float]]) -> np.ndarray:
+def create_v_matrix(problem: Problem, solutions: list[dict[str, float]]) -> np.ndarray:
     """Create the matrix V to form the e-cones.
 
     Args:
@@ -131,7 +163,12 @@ def e_cones(problem: Problem, solutions: list[dict[str, float]]) -> np.ndarray:
 
     return np.linalg.inv(matrix_v).T
 
-def create_milp(problem: Problem, approximation: np.ndarray, cones: np.ndarray | None = None) -> Problem:
+def create_milp(
+    problem: Problem,
+    approximation: np.ndarray,
+    v_matrix: np.ndarray | None = None,
+    solutions: np.ndarray | None = None
+) -> Problem:
     """Form the mixed integer linear problem to replace the original problem.
 
     The problem was defined in Hartikainen, M., Miettinen, K., & Wiecek, M. M. (2012). PAINT: Pareto front interpolation
@@ -140,7 +177,8 @@ def create_milp(problem: Problem, approximation: np.ndarray, cones: np.ndarray |
     Args:
         problem (Problem): The original problem.
         approximation (np.ndarray): The PAINT approximation to use in the surrogate.
-        cones (np.ndarray, optional): The e-cones.
+        v_matrix (np.ndarray, optional): The V matrix from e-cones.
+        solutions (np.ndarray, optional): A set of approximated solutions to form the e-cones around.
 
     Returns:
         Problem: A mixed integer linear problem to act as a surrogate problem for the original.
@@ -177,6 +215,7 @@ def create_milp(problem: Problem, approximation: np.ndarray, cones: np.ndarray |
     ind = 1 # as of now, an index to get the objective values from the polytopes of the approximation
     # a loop to form the objective funtions
     for obj in problem.objectives:
+        # form the sum of sums for each f_i
         sums = []
         for k in range(a):
             exprs = []
@@ -187,33 +226,18 @@ def create_milp(problem: Problem, approximation: np.ndarray, cones: np.ndarray |
             sums.append(expr)
         sums = " + ".join(sums)
 
-        # form the sum of sums for each f_i
-        if cones is None:
-            # form the sums into objectives
-            func = Objective(
-                name = f"f_{ind}",
-                symbol = f"f_{ind}",
-                func = sums,
-                objective_type = ObjectiveTypeEnum.analytical,
-                maximize = False,
-                ideal = ideal[obj.symbol],
-                nadir = nadir[obj.symbol]
-            )
-            objectives.append(func)
-            ind += 1
-        else:
-            # form the sums into objectives
-            func = Objective(
-                name = f"f_{ind}",
-                symbol = f"f_{ind}",
-                func = sums,
-                objective_type = ObjectiveTypeEnum.analytical,
-                maximize = False,
-                ideal = -float("inf"), # if there are e-cones, ideal is not assigned here but as separate constraints
-                nadir = float("inf")
-            )
-            objectives.append(func)
-            ind += 1
+        # form the sums into objectives
+        func = Objective(
+            name = f"f_{ind}",
+            symbol = f"f_{ind}",
+            func = sums,
+            objective_type = ObjectiveTypeEnum.analytical,
+            maximize = False,
+            ideal = ideal[obj.symbol],
+            nadir = nadir[obj.symbol]
+        )
+        objectives.append(func)
+        ind += 1
 
     constraints = []
     lambda_exprs = []
@@ -260,7 +284,23 @@ def create_milp(problem: Problem, approximation: np.ndarray, cones: np.ndarray |
     constraints.append(y_const)
 
     # add the constraints coming from the e-cones
-    if cones is not None:
+    # NOTE: do we need the PAINT approximation constraints defined above with these e-cone constraints?
+    if v_matrix is not None and solutions is not None:
+        for solution in solutions:
+            for i in range(len(problem.objectives)):
+                e_cone_exprs = []
+                for j in range(len(problem.objectives)):
+                    e_cone_exprs.append(f"{-cones[i][j]} * f_{j+1} + {solution[problem.objectives[j].symbol]}")
+                e_cone_expr = " + ".join(e_cone_exprs)
+                e_cone_con = Constraint(
+                    name=f"e_cone_con_{i}",
+                    symbol=f"e_cone_con_{i}",
+                    cons_type=ConstraintTypeEnum.LTE,
+                    func=e_cone_expr
+                )
+                constraints.append(e_cone_con)
+    # NOTE: remove this part when solutions is the actual set of solutions to use to form the e-cones around
+    elif v_matrix is not None and solutions is None:
         for i in range(len(problem.objectives)):
             e_cone_exprs = []
             for j in range(len(problem.objectives)):
@@ -321,7 +361,7 @@ def solve_next_solution(
     constraints = []
 
     for obj in problem.objectives:
-        constr = f"{obj.symbol} - {moved_reference_point_dict[obj.symbol]} - t"
+        constr = f"{obj.symbol}_min - {moved_reference_point_dict[obj.symbol]} - t"
 
         constraints.append(
             Constraint(
@@ -338,7 +378,6 @@ def solve_next_solution(
 
     solver = GurobipySolver(problem_w_t)
     res = solver.solve("t_func")
-    print(moved_reference_point, d)
     return res.optimal_objectives
 
 def calculate_all_solutions(
@@ -634,7 +673,7 @@ if __name__ == "__main__":
     milp = create_milp(problem, matrix)
     solutions = calculate_all_solutions(milp, starting_point, preference_information, adjusted_speed, 100)
 
-    cones = e_cones(milp, solutions)
+    cones = create_v_matrix(milp, solutions)
 
     milp = create_milp(milp, matrix, cones)
     starting_point = {"f_1": 16.08, "f_2": 425.2, "f_3": 11.0, "f_4": 14590, "f_5": 9132}
