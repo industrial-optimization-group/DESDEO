1d92c4e3f70063ef8934fd9196b6da3f16cc0baa
nieminme <mnieminen98@gmail.com>
2024-07-08 15:40:01 +0300

    Testing the forest problem implementation in comparison to the results gained with the rahti app.

--- desdeo/problem/testproblems.py
+++ desdeo/problem/testproblems.py
@@ -21,6 +21,7 @@ from desdeo.problem.schema import (
     Variable,
     VariableTypeEnum,
 )
+from pathlib import Path
 
 
 def binh_and_korn(maximize: tuple[bool] = (False, False)) -> Problem:
@@ -1118,7 +1119,7 @@ def simple_knapsack_vectors():
     )
 
 
-def forest_problem() -> Problem:
+def forest_problem(holding: int = 1, comparing: bool = False) -> Problem:
     r"""Defines a test forest problem that has TensorConstants and TensorVariables.
 
     The problem has TensorConstants V, W and P as vectors taking values from a data file and
@@ -1144,39 +1145,19 @@ def forest_problem() -> Problem:
     the net present value, wood volume at the end of the planning period, and the profit from harvesting
     are represented by $v_{ij}$, $w_{ij}$, and $p_{ij}$ respectively.
 
+    Args:
+        holding (int, optional): The number of the holding to be optimized. Defaults to 1.
+        comparing (bool, optional): Determines if solutions are to be compared to those from the rahti app.
+            Defaults to None.
+
     Returns:
         Problem: An instance of the test forest problem.
     """
-    df = pl.read_csv("./data/alternatives_290124.csv", dtypes={"unit": pl.Float64})
-    df_key = pl.read_csv("./data/alternatives_key_290124.csv", dtypes={"unit": pl.Float64})
+    df = pl.read_csv(Path(Path(__file__).parent.parent.parent) / "tests" / "data" / "alternatives_290124.csv", dtypes={"unit": pl.Float64})
+    df_key = pl.read_csv(Path(Path(__file__).parent.parent.parent) / "tests" / "data" / "alternatives_key_290124.csv", dtypes={"unit": pl.Float64})
 
-    selected_df_p = df.filter(pl.col("holding") == 2).select(["unit", "schedule", "harvest_value_period_2025", "harvest_value_period_2030", "harvest_value_period_2035"])
-    unique_units = selected_df_p.unique(["unit"], maintain_order=True).get_column("unit")
-    selected_df_p.group_by(["unit", "schedule"])
-    rows_by_key = selected_df_p.rows_by_key(key=["unit", "schedule"])
-    p_array = np.zeros((selected_df_p["unit"].n_unique(), selected_df_p["schedule"].n_unique()))
-    for i in range(np.shape(p_array)[0]):
-        for j in range(np.shape(p_array)[1]):
-            if (unique_units[i], j) in rows_by_key:
-                p_array[i][j] = sum(rows_by_key[(unique_units[i], j)][0])
-
-    selected_df_w = df.filter(pl.col("holding") == 2).select([ "unit", "schedule", "stock_2025", "stock_2030", "stock_2035"])
-    selected_df_w.group_by(["unit", "schedule"])
-    rows_by_key = selected_df_w.rows_by_key(key=["unit", "schedule"])
-    selected_df_key_w = df_key.select(["unit", "schedule", "treatment"])
-    selected_df_key_w.group_by(["unit", "schedule"])
-    rows_by_key_df_key = selected_df_key_w.rows_by_key(key=["unit", "schedule"])
-    w_array = np.zeros((selected_df_w["unit"].n_unique(), selected_df_w["schedule"].n_unique()))
-    for i in range(np.shape(w_array)[0]):
-        for j in range(np.shape(w_array)[1]):
-            if (unique_units[i], j) in rows_by_key and "2035" in rows_by_key_df_key[(unique_units[i], j)][0]:
-                w_array[i][j] = rows_by_key[(unique_units[i], j)][0][2]
-            elif (unique_units[i], j) in rows_by_key and "2030" in rows_by_key_df_key[(unique_units[i], j)][0]:
-                w_array[i][j] = rows_by_key[(unique_units[i], j)][0][1]
-            elif (unique_units[i], j) in rows_by_key and "2025" in rows_by_key_df_key[(unique_units[i], j)][0]:
-                w_array[i][j] = rows_by_key[(unique_units[i], j)][0][0]
-
-    selected_df_v = df.filter(pl.col("holding") == 2).select(["unit", "schedule", "npv_5_percent"])
+    selected_df_v = df.filter(pl.col("holding") == holding).select(["unit", "schedule", "npv_5_percent"])
+    unique_units = selected_df_v.unique(["unit"], maintain_order=True).get_column("unit")
     selected_df_v.group_by(["unit", "schedule"])
     rows_by_key = selected_df_v.rows_by_key(key=["unit", "schedule"])
     v_array = np.zeros((selected_df_v["unit"].n_unique(), selected_df_v["schedule"].n_unique()))
@@ -1185,26 +1166,60 @@ def forest_problem() -> Problem:
             if (unique_units[i], j) in rows_by_key:
                 v_array[i][j] = rows_by_key[(unique_units[i], j)][0]
 
-    constants = []
-    for i in range(np.shape(w_array)[0]):
-        w = TensorConstant(
-            name=f"W_{i+1}",
-            symbol=f"W_{i+1}",
-            shape=[np.shape(w_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
-            values=w_array[i].tolist()
-        )
-        constants.append(w)
+    # determine whether the results are to be compared to those from the rahti app (for testing purposes)
+    # if compared, the stock values are calculated by substacting the value after 2025 period from
+    # the value after the 2035 period (in other words, last value - first value)
+    if comparing:
+        selected_df_w = df.filter(pl.col("holding") == holding).select([ "unit", "schedule", "stock_2025", "stock_2030", "stock_2035"])
+        selected_df_w.group_by(["unit", "schedule"])
+        rows_by_key = selected_df_w.rows_by_key(key=["unit", "schedule"])
+        selected_df_key_w = df_key.select(["unit", "schedule", "treatment"])
+        selected_df_key_w.group_by(["unit", "schedule"])
+        rows_by_key_df_key = selected_df_key_w.rows_by_key(key=["unit", "schedule"])
+        w_array = np.zeros((selected_df_w["unit"].n_unique(), selected_df_w["schedule"].n_unique()))
+        for i in range(np.shape(w_array)[0]):
+            for j in range(np.shape(w_array)[1]):
+                if len(rows_by_key_df_key[(unique_units[i], j)]) == 0:
+                    continue
+                if (unique_units[i], j) in rows_by_key:
+                    w_array[i][j] = rows_by_key[(unique_units[i], j)][0][2] - rows_by_key[(unique_units[i], j)][0][0]
+    else:
+        selected_df_w = df.filter(pl.col("holding") == holding).select([ "unit", "schedule", "stock_2025", "stock_2030", "stock_2035"])
+        selected_df_w.group_by(["unit", "schedule"])
+        rows_by_key = selected_df_w.rows_by_key(key=["unit", "schedule"])
+        selected_df_key_w = df_key.select(["unit", "schedule", "treatment"])
+        selected_df_key_w.group_by(["unit", "schedule"])
+        rows_by_key_df_key = selected_df_key_w.rows_by_key(key=["unit", "schedule"])
+        w_array = np.zeros((selected_df_w["unit"].n_unique(), selected_df_w["schedule"].n_unique()))
+        for i in range(np.shape(w_array)[0]):
+            for j in range(np.shape(w_array)[1]):
+                if len(rows_by_key_df_key[(unique_units[i], j)]) == 0:
+                    continue
+                if (unique_units[i], j) in rows_by_key and "2035" in rows_by_key_df_key[(unique_units[i], j)][0]:
+                    w_array[i][j] = rows_by_key[(unique_units[i], j)][0][2]
+                elif (unique_units[i], j) in rows_by_key and "2030" in rows_by_key_df_key[(unique_units[i], j)][0]:
+                    w_array[i][j] = rows_by_key[(unique_units[i], j)][0][1]
+                elif (unique_units[i], j) in rows_by_key and "2025" or "donothing" in rows_by_key_df_key[(unique_units[i], j)][0]:
+                    w_array[i][j] = rows_by_key[(unique_units[i], j)][0][0]
 
+    selected_df_p = df.filter(pl.col("holding") == holding).select(["unit", "schedule", "harvest_value_period_2025", "harvest_value_period_2030", "harvest_value_period_2035"])
+    selected_df_p.group_by(["unit", "schedule"])
+    rows_by_key = selected_df_p.rows_by_key(key=["unit", "schedule"])
+    p_array = np.zeros((selected_df_p["unit"].n_unique(), selected_df_p["schedule"].n_unique()))
     for i in range(np.shape(p_array)[0]):
-        p = TensorConstant(
-            name=f"P_{i+1}",
-            symbol=f"P_{i+1}",
-            shape=[np.shape(p_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
-            values=p_array[i].tolist()
-        )
-        constants.append(p)
+        for j in range(np.shape(p_array)[1]):
+            if (unique_units[i], j) in rows_by_key:
+                p_array[i][j] = sum(rows_by_key[(unique_units[i], j)][0])
 
+    constants = []
+    variables = []
+    constraints = []
+    f_1_func = []
+    f_2_func = []
+    f_3_func = []
+    # define the constants V, W and P, decision variable X, constraints, and objective function expressions in one loop
     for i in range(np.shape(v_array)[0]):
+        # Constants V, W and P
         v = TensorConstant(
             name=f"V_{i+1}",
             symbol=f"V_{i+1}",
@@ -1212,22 +1227,34 @@ def forest_problem() -> Problem:
             values=v_array[i].tolist()
         )
         constants.append(v)
+        w = TensorConstant(
+            name=f"W_{i+1}",
+            symbol=f"W_{i+1}",
+            shape=[np.shape(w_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
+            values=w_array[i].tolist()
+        )
+        constants.append(w)
+        p = TensorConstant(
+            name=f"P_{i+1}",
+            symbol=f"P_{i+1}",
+            shape=[np.shape(p_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
+            values=p_array[i].tolist()
+        )
 
-    variables = []
-    for i in range(np.shape(p_array)[0]):
+        # Decision variable X
+        constants.append(p)
         x = TensorVariable(
             name=f"X_{i+1}",
             symbol=f"X_{i+1}",
             variable_type=VariableTypeEnum.binary,
-            shape=[np.shape(p_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
-            lowerbounds=np.shape(p_array)[1] * [0],
-            upperbounds=np.shape(p_array)[1] * [1],
-            initial_values=np.shape(p_array)[1] * [0]
+            shape=[np.shape(v_array)[1]], # NOTE: vectors have to be of form [2] instead of [2,1] or [1,2]
+            lowerbounds=np.shape(v_array)[1] * [0],
+            upperbounds=np.shape(v_array)[1] * [1],
+            initial_values=np.shape(v_array)[1] * [0]
         )
         variables.append(x)
 
-    constraints = []
-    for i in range(np.shape(p_array)[0]):
+        # Constraints
         con = Constraint(
             name=f"x_con_{i+1}",
             symbol=f"x_con_{i+1}",
@@ -1236,11 +1263,20 @@ def forest_problem() -> Problem:
         )
         constraints.append(con)
 
-    f_1_func = []
-    for i in range(np.shape(v_array)[0]):
+        # Objective function expressions
         exprs = f"V_{i+1}@X_{i+1}"
         f_1_func.append(exprs)
+
+        exprs = f"W_{i+1}@X_{i+1}"
+        f_2_func.append(exprs)
+
+        exprs = f"P_{i+1}@X_{i+1}"
+        f_3_func.append(exprs)
+
+    # form the objective function sums
     f_1_func = " + ".join(f_1_func)
+    f_2_func = " + ".join(f_2_func)
+    f_3_func = " + ".join(f_3_func)
 
     f_1 = Objective(
         name="Net present value",
@@ -1253,12 +1289,6 @@ def forest_problem() -> Problem:
         is_twice_differentiable=True
     )
 
-    f_2_func = []
-    for i in range(np.shape(w_array)[0]):
-        exprs = f"W_{i+1}@X_{i+1}"
-        f_2_func.append(exprs)
-    f_2_func = " + ".join(f_2_func)
-
     f_2 = Objective(
         name="f_2",
         symbol="f_2",
@@ -1269,12 +1299,6 @@ def forest_problem() -> Problem:
         is_convex=False, # not checked
         is_twice_differentiable=True
     )
-
-    f_3_func = []
-    for i in range(np.shape(p_array)[0]):
-        exprs = f"P_{i+1}@X_{i+1}"
-        f_3_func.append(exprs)
-    f_3_func = " + ".join(f_3_func)
 
     f_3 = Objective(
         name="f_3",
@@ -1300,12 +1324,12 @@ def forest_problem() -> Problem:
 if __name__ == "__main__":
     #problem = simple_scenario_test_problem()
     #print(problem.model_dump_json(indent=2))
-    from desdeo.problem import PyomoEvaluator, GurobipyEvaluator
     from desdeo.tools import GurobipySolver
-    problem = forest_problem()
+
+    problem = forest_problem(holding=5)
     #problem = simple_knapsack_vectors()
     #problem = simple_linear_test_problem()
     evaluator = GurobipySolver(problem)
-    res = evaluator.solve("f_2_min")
+    res = evaluator.solve("f_3_min")
     print(res.optimal_variables, res.optimal_objectives, res.constraint_values)
     #print(res.optimal_objectives)
