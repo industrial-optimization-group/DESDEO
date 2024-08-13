cede010b2b3c641fc5229e097f7b648589fb9c7f
Juho Roponen <juho.o.roponen@jyu.fi>
2024-08-13 13:20:09 +0300

    added initial version of s2i config

--- desdeo/api/config.py
+++ desdeo/api/config.py
@@ -6,26 +6,26 @@ from pydantic import (
     StrictInt,
     StrictBool
 )
-from typing import Optional, Union
+from typing import ClassVar, Optional, Union
 from datetime import timedelta
 
 class AuthConfig(BaseModel):
     """General configurations."""
 
     # openssl rand -hex 32
-    authjwt_secret_key: Optional[StrictStr] = "36b96a23d24cebdeadce6d98fa53356111e6f3e85b8144d7273dcba230b9eb18"
-    authjwt_algorithm: Optional[StrictStr] = "HS256"
-    authjwt_access_token_expires: Optional[StrictInt] = 15 # in minutes
-    authjwt_refresh_token_expires: Optional[StrictInt] = 30 # in minutes
+    authjwt_secret_key: ClassVar[StrictStr] = "36b96a23d24cebdeadce6d98fa53356111e6f3e85b8144d7273dcba230b9eb18"
+    authjwt_algorithm: ClassVar[StrictStr] = "HS256"
+    authjwt_access_token_expires: ClassVar[StrictInt] = 15 # in minutes
+    authjwt_refresh_token_expires: ClassVar[StrictInt] = 30 # in minutes
 
 class DBConfig(BaseModel):
     """Database configurations."""
 
-    db_host: Optional[StrictStr] = "localhost"
-    db_port: Optional[StrictStr] = "5432"
-    db_database: Optional[StrictStr] = "DESDEO3"
-    db_username: Optional[StrictStr] = "bhupindersaini"
-    db_password: Optional[StrictStr] = ""
-    db_pool_size: Optional[StrictInt] = 20
-    db_max_overflow: Optional[StrictInt] = 20
-    db_pool: Optional[StrictBool] = True
+    db_host: ClassVar[StrictStr] = "86.50.253.131"
+    db_port: ClassVar[StrictStr] = "5432"
+    db_database: ClassVar[StrictStr] = "test"
+    db_username: ClassVar[StrictStr] = "test"
+    db_password: ClassVar[StrictStr] = "testdb"
+    db_pool_size: ClassVar[StrictInt] = 20
+    db_max_overflow: ClassVar[StrictInt] = 20
+    db_pool: ClassVar[StrictBool] = True
