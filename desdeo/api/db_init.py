2a48188035881b15fb0b4179cfe4277b3f8e51f5
Juho Roponen <juho.o.roponen@juy.fi>
2024-08-14 15:11:26 +0300

    Comment role_permission out for now

--- desdeo/api/db_init.py
+++ desdeo/api/db_init.py
@@ -61,7 +61,7 @@ problem_in_db = db_models.Problem(
     kind=ProblemKind.CONTINUOUS,
     obj_kind=ObjectiveKind.ANALYTICAL,
     value=problem.model_dump(mode="json"),
-    role_permission=[],
+    # role_permission=[],
 )
 
 db.add(problem_in_db)
