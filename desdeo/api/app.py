44c9e7533e9040e01fcd35b992d58b3f24e79c6d
Juho Roponen <juho.o.roponen@juy.fi>
2024-08-14 17:40:34 +0300

    This should maybe work

--- desdeo/api/app.py
+++ desdeo/api/app.py
@@ -19,11 +19,12 @@ app.include_router(problems.router)
 app.include_router(NAUTILUS_navigator.router)
 app.include_router(NAUTILUS.router)
 
-origins = ["*"]
+origins = ["http://localhost", "http://localhost:8080"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.2\.rahtiapp\.fi",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
