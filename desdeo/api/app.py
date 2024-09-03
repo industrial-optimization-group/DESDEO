c4e0956e0b3455ce06bdc3deee0997fa04d60553
Hoang Bich Tram Mai <maihbt@jyu-mac-20240520.local>
2024-09-03 13:49:19 +0300

    Add gnimbus router

--- desdeo/api/app.py
+++ desdeo/api/app.py
@@ -4,7 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from desdeo.api.routers import NIMBUS, NAUTILUS_navigator, UserAuth, problems, test, NAUTILUS
+from desdeo.api.routers import NIMBUS, GNIMBUS, NAUTILUS_navigator, UserAuth, problems, test, NAUTILUS
 from desdeo.api.config import WebUIConfig
 
 app = FastAPI(
@@ -14,6 +14,7 @@ app = FastAPI(
 )
 
 app.include_router(NIMBUS.router)
+app.include_router(GNIMBUS.router)
 app.include_router(test.router)
 app.include_router(UserAuth.router)
 app.include_router(problems.router)
