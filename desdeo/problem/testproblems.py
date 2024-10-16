bdd73a1ee39f15ca74f8bb28507278a1f8d74df8
Bhupinder Saini <bhupinder.s.saini@jyu.fi>
2024-10-16 11:13:58 +0300

    Fixed DTLZ2 nadir

--- desdeo/problem/testproblems.py
+++ desdeo/problem/testproblems.py
@@ -789,7 +789,7 @@ def dtlz2(n_variables: int, n_objectives: int) -> Problem:
                 func=f_m_expr,
                 maximize=False,
                 ideal=0,
-                nadir=2,  # Assuming the range of g and the trigonometric functions
+                nadir=1,  # Assuming the range of g and the trigonometric functions
                 is_convex=False,
                 is_linear=False,
                 is_twice_differentiable=True,
