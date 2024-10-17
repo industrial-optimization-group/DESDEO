cd50bf9b1cdd1934290638b2edc8b534e9934699
nieminme <mnieminen98@gmail.com>
2024-10-17 15:22:30 +0300

    Some refactoring.

--- desdeo/problem/simulator_evaluator.py
+++ desdeo/problem/simulator_evaluator.py
@@ -36,51 +36,59 @@ class Evaluator:
             params (dict[str, dict], optional): Parameters for the different simulators used in the problem.
                 Given as dict with the simulators' symbols as keys and the corresponding simulator parameters
                 as a dict as values. Defaults to None.
-            surrogate_paths (dict[str, Path]): A dictionary where the keys are the names of the objectives, constraints
-                and extra functions and the values are the paths to the surrogate models saved on disk. The names of
-                the objectives, constraints and extra functions should match the names of the objectives, constraints and
-                extra functions in the problem JSON. Defaults to None.
+            surrogate_paths (dict[str, Path], optional): A dictionary where the keys are the names of the objectives,
+                constraints and extra functions and the values are the paths to the surrogate models saved on disk.
+                The names of the objectives, constraints and extra functions should match the names of the objectives,
+                constraints and extra functions in the problem JSON. Defaults to None.
         """
         self.problem = problem
-        # Gather the problem's objectives
-        self.problem_objectives = problem.objectives
         # store the symbol and min or max multiplier as well (symbol, min/max multiplier [1 | -1])
         self.objective_mix_max_mult = [
             (objective.symbol, -1 if objective.maximize else 1)
-            for objective in self.problem_objectives
+            for objective in problem.objectives
         ]
-        self.objective_symbols = [obj.symbol for obj in problem.objectives]
-        # Gather objectives of different types into their own lists
-        self.analytical_objectives = list(filter(lambda x: x.objective_type == ObjectiveTypeEnum.analytical, problem.objectives))
-        #self.data_based_objectives = list(filter(lambda x: x.objective_type == ObjectiveTypeEnum.data_based, problem.objectives))
-        self.simulator_objectives = list(filter(lambda x: x.objective_type == ObjectiveTypeEnum.simulator, problem.objectives))
-        self.surrogate_objectives = list(filter(lambda x: x.objective_type == ObjectiveTypeEnum.surrogate, problem.objectives))
-        # Gather any constraints
-        self.analytical_constraints = []
-        self.simulator_constraints = []
-        self.surrogate_constraints = []
-        self.constraint_symbols = []
-        self.problem_constraints = problem.constraints
+        # Gather symbols objectives of different types into their own lists
+        self.analytical_symbols = [
+            obj.symbol for obj in list(
+                filter(lambda x: x.objective_type == ObjectiveTypeEnum.analytical, problem.objectives)
+            )
+        ]
+        self.simulator_symbols = [
+            obj.symbol for obj in list(
+                filter(lambda x: x.objective_type == ObjectiveTypeEnum.simulator, problem.objectives)
+            )
+        ]
+        self.surrogate_symbols = [
+            obj.symbol for obj in list(
+                filter(lambda x: x.objective_type == ObjectiveTypeEnum.surrogate, problem.objectives)
+            )
+        ]
+        # Gather any constraints' symbols
         if problem.constraints is not None:
-            self.analytical_constraints = list(filter(lambda x: x.func is not None, problem.constraints))
-            self.simulator_constraints = list(filter(lambda x: x.simulator_path is not None, problem.constraints))
-            self.surrogate_constraints = list(filter(lambda x: x.surrogate is not None, problem.constraints))
-            self.constraint_symbols = [con.symbol for con in problem.constraints]
+            self.analytical_symbols = self.analytical_symbols + [
+                con.symbol for con in list(filter(lambda x: x.func is not None, problem.constraints))
+            ]
+            self.simulator_symbols = self.simulator_symbols + [
+                con.symbol for con in list(filter(lambda x: x.simulator_path is not None, problem.constraints))
+            ]
+            self.surrogate_symbols = self.surrogate_symbols + [
+                con.symbol for con in list(filter(lambda x: x.surrogate is not None, problem.constraints))
+            ]
 
-        # Gather any extra functions
-        self.analytical_extras = []
-        self.simulator_extras = []
-        self.surrogate_extras = []
-        self.extra_symbols = []
-        self.problem_extras = problem.extra_funcs
+        # Gather any extra functions' symbols
         if problem.extra_funcs is not None:
-            self.analytical_extras = list(filter(lambda x: x.func is not None, problem.extra_funcs))
-            self.simulator_extras = list(filter(lambda x: x.simulator_path is not None, problem.extra_funcs))
-            self.surrogate_extras = list(filter(lambda x: x.surrogate is not None, problem.extra_funcs))
-            self.extra_symbols = [extra.symbol for extra in problem.extra_funcs]
+            self.analytical_symbols = self.analytical_symbols + [
+                extra.symbol for extra in list(filter(lambda x: x.func is not None, problem.extra_funcs))
+            ]
+            self.simulator_symbols = self.simulator_symbols + [
+                extra.symbol for extra in list(filter(lambda x: x.simulator_path is not None, problem.extra_funcs))
+            ]
+            self.surrogate_symbols = self.surrogate_symbols + [
+                extra.symbol for extra in list(filter(lambda x: x.surrogate is not None, problem.extra_funcs))
+            ]
 
         # Gather all the symbols of objectives, constraints and extra functions
-        self.problem_symbols = self.objective_symbols + self.constraint_symbols + self.extra_symbols
+        self.problem_symbols = self.analytical_symbols + self.simulator_symbols + self.surrogate_symbols
 
         # Gather the possible simulators
         self.simulators = problem.simulators
@@ -109,40 +117,25 @@ class Evaluator:
                 and the length of the columns is the number of samples. Will return those objective, constraint and
                 extra function values that are gained from simulators listed in the problem object.
         """
-        results = {}
-        sim_symbols = []
+        res_df = pl.DataFrame()
         for sim in self.simulators:
-            for obj in self.simulator_objectives:
-                if obj.simulator_path == sim.file:
-                    sim_symbols.append(obj.symbol)
-
-            if self.problem_constraints is not None:
-                for con in self.problem_constraints:
-                    if con.simulator_path == sim.file:
-                        sim_symbols.append(con.symbol)
-
-            if self.problem_extras is not None:
-                for extra in self.problem_extras:
-                    if extra.simulator_path == sim.file:
-                        sim_symbols.append(extra.symbol)
-
+            # gather the possible parameters for the simulator
             params = self.params.get(sim.name, {})
+            # call the simulator with the decision variable values and parameters as dicts
             res = subprocess.run(
                 f"{sys.executable} {sim.file} -d {xs} -p {params}", check=True, capture_output=True
             ).stdout.decode()
-            res = dict(json.loads(''.join(res)))
-            for key, value in res.items():
-                results[key] = value
-        res = pl.DataFrame(results)
+            # gather the simulation results (a dict) into the results dataframe
+            res_df = res_df.hstack(pl.DataFrame(json.loads(res)))
 
         # Evaluate the minimization form of the objective functions
         min_obj_columns = pl.DataFrame()
         for symbol, min_max_mult in self.objective_mix_max_mult:
-            if symbol in res.columns:
+            if symbol in res_df.columns:
                 min_obj_columns = min_obj_columns.hstack(
-                    res.select((min_max_mult * pl.col(f"{symbol}")).alias(f"{symbol}_min"))
+                    res_df.select((min_max_mult * pl.col(f"{symbol}")).alias(f"{symbol}_min"))
                 )
-        return res.hstack(min_obj_columns)
+        return res_df.hstack(min_obj_columns)
 
     def _evaluate_surrogates(self, xs: dict[str, list[int | float]]) -> pl.DataFrame:
         """Evaluate the problem for the given decision variables using the surrogate models.
@@ -158,8 +151,8 @@ class Evaluator:
                 dataframe. The uncertainty prediction values are also returned. If a model does not provide
                 uncertainty predictions, then they are set as NaN.
         """
-        results_dict = {}
-        var = np.array([value for _, value in xs.items()]).T # has to be transpose (m, n) at least for sklearn models
+        res = pl.DataFrame()
+        var = np.array([value for _, value in xs.items()]).T # has to be transpose (at least for sklearn models)
         for symbol in self.surrogates:
             # get a list of args accepted by the model's predict function
             accepted_args = getfullargspec(self.surrogates[symbol].predict).args
@@ -170,13 +163,12 @@ class Evaluator:
             else:
                 value = self.surrogates[symbol].predict(var)
                 uncertainty = np.full(np.shape(value), np.nan)
-            # add the objects, constraints and extra functions into a dict that is then turned into a polars dataframe
+            # add the objects, constraints and extra functions into the polars dataframe
             # values go into columns with the symbol as the column names
-            results_dict[symbol] = value
+            res = res.with_columns(pl.Series(value).alias(symbol))
             # uncertainties go into columns with {symbol}_uncert as the column names
-            results_dict[f"{symbol}_uncert"] = uncertainty
+            res = res.with_columns(pl.Series(uncertainty).alias(f"{symbol}_uncert"))
 
-        res = pl.DataFrame(results_dict)
         # Evaluate the minimization form of the objective functions
         min_obj_columns = pl.DataFrame()
         for symbol, min_max_mult in self.objective_mix_max_mult:
@@ -227,23 +219,18 @@ class Evaluator:
         res = pl.DataFrame()
 
         # Evaluate the analytical functions
-        if len(self.analytical_objectives + self.analytical_constraints + self.analytical_extras) > 0:
+        if len(self.analytical_symbols) > 0:
             polars_evaluator = PolarsEvaluator(self.problem, evaluator_mode=PolarsEvaluatorModesEnum.mixed)
             analytical_values = polars_evaluator._polars_evaluate(xs)
             res = res.hstack(analytical_values)
 
-        """data_evaluator = GenericEvaluator(self.problem, evaluator_mode=EvaluatorModesEnum.mixed)
-        data_values = data_evaluator._from_discrete_data()
-        obj_values.append(data_values)"""
-        #data_objs = self._from_discrete_data()
-
         # Evaluate the simulator based functions
-        if len(self.simulator_objectives + self.simulator_constraints + self.simulator_extras) > 0:
+        if len(self.simulator_symbols) > 0:
             simulator_values = self._evaluate_simulator(xs)
             res = res.hstack(simulator_values)
 
         # Evaluate the surrogate based functions
-        if len(self.surrogate_objectives + self.surrogate_constraints + self.simulator_extras) > 0:
+        if len(self.surrogate_symbols) > 0:
             surrogate_values = self._evaluate_surrogates(xs)
             res = res.hstack(surrogate_values)
 
@@ -254,7 +241,7 @@ class Evaluator:
         return res
 
 if __name__ == "__main__":
-    from desdeo.problem import simulator_problem, surrogate_problem
+    from desdeo.problem import simple_knapsack_vectors, simple_linear_test_problem, simulator_problem, surrogate_problem
 
     evaluator = Evaluator(
         simulator_problem(),
@@ -263,16 +250,12 @@ if __name__ == "__main__":
                          "f_6": Path("model2.skops"),
                          "g_3": Path("model.skops"),
                          "e_3": Path("model2.skops")})
-    """res = evaluator.evaluate({
-        "analytical": {"x_1": [0, 1, 2, 3, 4], "x_2": [4, 3, 2, 1, 0], "x_3": [0, 4, 1, 3, 2]},
-        "simulator": np.array([[0, 1, 2, 3, 4], [4, 3, 2, 1, 0], [0, 4, 1, 3, 2], [3, 1, 3, 2, 3]]),
-        "surrogate": np.array([[0, 1, 2, 3, 4], [4, 3, 2, 1, 0], [0, 4, 1, 3, 2], [3, 1, 3, 2, 3]])})"""
     res = evaluator.evaluate({
         "x_1": [0, 1, 2, 3, 4],
         "x_2": [4, 3, 2, 1, 0],
         "x_3": [0, 4, 1, 3, 2],
         "x_4": [3, 1, 3, 2, 3]})
-    #res = evaluator._evaluate_simulator(np.array([[0, 1, 2, 3], [4, 3, 2, 1], [0, 4, 1, 3]]))
-    #print(np.shape(res), res)
+    """evaluator = Evaluator(simple_knapsack_vectors())
+    res = evaluator.evaluate(xs={"X": [1,1,1,1]})"""
     with pl.Config(tbl_cols=-1):
         print(res)
