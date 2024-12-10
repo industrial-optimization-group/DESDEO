c24f628e2280f7220b543a5356c03ef2169535b4
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2024-12-10 17:09:17 +0200

    Explanation changes.
    - Progress on SHAP values.

--- desdeo/explanations/explainer.py
+++ desdeo/explanations/explainer.py
@@ -1,9 +1,12 @@
 """Explainers are defined here."""
 
-from typing import Callable
+from collections.abc import Callable
 
+import numpy as np
 import polars as pl
 import shap
+
+from desdeo.problem import Problem
 
 
 class BlackBoxError(Exception):
@@ -11,23 +14,30 @@ class BlackBoxError(Exception):
 
 
 class BlackBox:
-    """The 'BlackBox' class defines a model that should be explained."""
+    """The 'BlackBox' class defines a model that should be explained.
+
+    The black box is used as a wrapper to abstract the input to an interactive method,
+    e.g., a reference point, and the method's output.
+    """
 
     def __init__(
-        self, evaluator: Callable[[pl.DataFrame | dict, ...], pl.DataFrame], evaluator_kwargs: dict | None = None
+        self,
+        problem: Problem,
+        evaluator: Callable[[np.ndarray, ...], np.ndarray],
+        evaluator_kwargs: dict | None = None,
     ):
         self.evaluator = evaluator
         self.evaluator_kwargs = evaluator_kwargs
 
-    def evaluate(self, xs: dict) -> pl.DataFrame:
+    def evaluate(self, xs: np.ndarray) -> np.ndarray:
         """Evaluate the black box.
 
         Args:
-            xs (dict):
+            xs (np.ndarray):
 
 
         Returns:
-            pl.DataFrame: returns a Polars dataframe with the values returned by evaluating
+            np.ndarray: returns a numpy array with the values returned by evaluating
                 the black box
         """
         # needs to account for calling argument like
@@ -35,14 +45,21 @@ class BlackBox:
         # [0.54925335 0.92982054 0.32396162]
         # ...
         # [0.57616443 1.23657878 0.23357839]
-        return self.evaluator(xs, **self.evaluator_kwargs) if self.evaluator_kwargs is not None else self.evaluator(xs)
+
+        return np.apply_along_axis(
+            lambda x: self.evaluator(x, **self.evaluator_kwargs)
+            if self.evaluator_kwargs is not None
+            else self.evaluator(x),
+            axis=1,
+            arr=np.atleast_2d(xs),
+        )
 
 
 class ShapExplainer:
-    def __init__(self, bb: BlackBox, missing_data: pl.DataFrame):
+    def __init__(self, bb: BlackBox, missing_data: np.ndarray):
         self.bb = bb
-        self.missing_data = missing_data
-        self.explainer = shap.explainers.Permutation(self.bb.evaluate, self.missing_data.to_numpy())
+        self.missing_data = np.atleast_2d(missing_data)
+        self.explainer = shap.explainers.KernelExplainer(self.bb.evaluate, self.missing_data)
 
-    def shap_values(self, x: dict):
-        return self.explainer(x)
+    def shap_values(self, x: np.ndarray):
+        return self.explainer(np.atleast_2d(x))
--- tests/test_explainer.py
+++ tests/test_explainer.py
@@ -5,51 +5,75 @@ import pytest
 from desdeo.emo.operators.generator import LHSGenerator
 from desdeo.emo.operators.evaluator import EMOEvaluator
 from desdeo.explanations.explainer import BlackBox, ShapExplainer
-from desdeo.problem.testproblems import dtlz2
+from desdeo.problem.testproblems import simple_data_problem
 from desdeo.mcdm import rpm_solve_solutions
-from desdeo.tools import PyomoBonminSolver
+from desdeo.tools import ProximalSolver
+from desdeo.problem import numpy_array_to_objective_dict, objective_dict_to_numpy_array
 from desdeo.tools.patterns import Publisher
 
 import polars as pl
+import numpy as np
 
 
 @pytest.mark.explainer
 def test_blackbox():
     """Test the black box works as expected."""
-    problem = dtlz2(5, 3)
+    problem = simple_data_problem()
 
-    def evaluator(reference_point, problem, solver):
-        results = rpm_solve_solutions(problem=problem, reference_point=reference_point, solver=solver)
-        return pl.DataFrame([res.optimal_objectives for res in results])
+    def evaluator(reference_point: np.ndarray, problem, solver):
+        results = rpm_solve_solutions(
+            problem=problem,
+            reference_point=numpy_array_to_objective_dict(problem=problem, numpy_array=reference_point),
+            solver=solver,
+        )
+        return np.array(
+            [objective_dict_to_numpy_array(problem=problem, objective_dict=res.optimal_objectives) for res in results]
+        )
 
-    bb = BlackBox(evaluator, {"problem": problem, "solver": PyomoBonminSolver})
+    bb = BlackBox(problem, evaluator, {"problem": problem, "solver": ProximalSolver})
 
-    ref_point = {"f_1": 0.5, "f_2": 0.7, "f_3": 0.4}
+    # single input
+    ref_point = objective_dict_to_numpy_array(problem, {"g_1": 2500.0, "g_2": 7.5, "g_3": -30.0})
 
     res = bb.evaluate(ref_point)
 
-    assert "f_1" in res
-    assert "f_2" in res
-    assert "f_3" in res
+    assert res.shape == (1, len(problem.objectives) + 1, len(problem.objectives))  # num inputs, k+1, num objectives
+
+    # multi input
+    ref_point_multi = np.array([[2500.0, 7.5, -30.0], [1500.0, 2.0, -50], [2500.0, 7.5, -30.0]])
+
+    res = bb.evaluate(ref_point_multi)
+
+    assert res.shape == (
+        len(ref_point_multi),
+        len(problem.objectives) + 1,
+        len(problem.objectives),
+    )  # num inputs, k+1, num objectives
 
 
 @pytest.mark.explainer
 def test_shap_explainer():
     """Test the SHAP explainer."""
-    problem = dtlz2(5, 3)
+    problem = simple_data_problem()
 
-    def evaluator(reference_point, problem, solver):
-        results = rpm_solve_solutions(problem=problem, reference_point=reference_point, solver=solver)
-        return pl.DataFrame([res.optimal_objectives for res in results])
+    def evaluator(reference_point: np.ndarray, problem, solver):
+        results = rpm_solve_solutions(
+            problem=problem,
+            reference_point=numpy_array_to_objective_dict(problem=problem, numpy_array=reference_point),
+            solver=solver,
+        )
+        return np.array(objective_dict_to_numpy_array(problem=problem, objective_dict=results[0].optimal_objectives))
 
-    bb = BlackBox(evaluator, {"problem": problem, "solver": PyomoBonminSolver})
+    bb = BlackBox(problem, evaluator, {"problem": problem, "solver": ProximalSolver})
 
     publisher = Publisher()
     emo_evaluator = EMOEvaluator(problem, publisher=publisher)
-    generator = LHSGenerator(problem, emo_evaluator, n_points=10000, seed=0, publisher=publisher)
+    generator = LHSGenerator(problem, emo_evaluator, n_points=10, seed=0, publisher=publisher)
 
     missing_data = generator.do()[1][[objective.symbol for objective in problem.objectives]]
 
-    explainer = ShapExplainer(bb, missing_data)
+    explainer = ShapExplainer(bb, objective_dict_to_numpy_array(problem, problem.get_nadir_point()))
+
+    res = explainer.shap_values(np.array([2600.0, 5.5, -25.0]))
 
     print()
