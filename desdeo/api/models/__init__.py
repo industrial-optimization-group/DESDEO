00a1e7b31ee76d8354e15b558ad305576e4b21c6
Stina Palomäki <palomakistina@gmail.com>
2025-06-12 15:55:06 +0300

    rename and restructure archive models

--- desdeo/api/models/__init__.py
+++ desdeo/api/models/__init__.py
@@ -1,8 +1,8 @@
 """Model exports."""
 
 __all__ = [
-    "ArchiveEntryBase",
-    "ArchiveEntryDB",
+    "UserSavedSolutionBase",
+    "UserSavedSolutionDB",
     "Bounds",
     "ConstantDB",
     "ConstraintDB",
@@ -18,6 +18,8 @@ __all__ = [
     "NIMBUSBaseState",
     "NIMBUSClassificationRequest",
     "NIMBUSClassificationState",
+    "NIMBUSSaveRequest",
+    "NIMBUSSaveState",
     "ObjectiveDB",
     "PreferenceBase",
     "PreferenceDB",
@@ -41,9 +43,9 @@ __all__ = [
     "VariableDB",
 ]
 
-from .archive import ArchiveEntryBase, ArchiveEntryDB
+from .archive import UserSavedSolutionBase, UserSavedSolutionDB
 from .generic import IntermediateSolutionRequest
-from .nimbus import NIMBUSClassificationRequest
+from .nimbus import NIMBUSClassificationRequest, NIMBUSSaveRequest
 from .preference import Bounds, PreferenceBase, PreferenceDB, ReferencePoint
 from .problem import (
     ConstantDB,
@@ -73,6 +75,7 @@ from .state import (
     IntermediateSolutionState,
     NIMBUSBaseState,
     NIMBUSClassificationState,
+    NIMBUSSaveState,
     RPMBaseState,
     RPMState,
     StateDB,
--- desdeo/api/models/archive.py
+++ desdeo/api/models/archive.py
@@ -4,14 +4,14 @@ from typing import TYPE_CHECKING
 
 from sqlmodel import JSON, Column, Field, Relationship, SQLModel
 
-from .preference import PreferenceDB
 
 if TYPE_CHECKING:
     from .problem import ProblemDB
+    from .state import StateDB
     from .user import User
 
 
-class ArchiveEntryBase(SQLModel):
+class UserSavedSolutionBase(SQLModel):
     """The base model of an archive entry."""
 
     variable_values: dict[str, float | list] = Field(sa_column=Column(JSON, nullable=False))
@@ -19,16 +19,15 @@ class ArchiveEntryBase(SQLModel):
     constraint_values: dict[str, float] | None = Field(sa_column=Column(JSON), default=None)
     extra_func_values: dict[str, float] | None = Field(sa_column=Column(JSON), default=None)
 
-
-class ArchiveEntryDB(ArchiveEntryBase, table=True):
+class UserSavedSolutionDB(UserSavedSolutionBase, table=True):
     """Database model of an archive entry."""
 
     id: int | None = Field(primary_key=True, default=None)
+    name: str | None = Field(default=None, nullable=True)  # Optional name for the solution
     user_id: int | None = Field(foreign_key="user.id", default=None)
     problem_id: int | None = Field(foreign_key="problemdb.id", default=None)
-    preference_id: int | None = Field(foreign_key="preferencedb.id", default=None)
-
+    state_id: int | None = Field(foreign_key="statedb.id", default=None)
     # Back populates
     user: "User" = Relationship(back_populates="archive")
-    preference: "PreferenceDB" = Relationship(back_populates="solutions")
     problem: "ProblemDB" = Relationship(back_populates="solutions")
+    state: "StateDB" = Relationship(back_populates="solutions")
