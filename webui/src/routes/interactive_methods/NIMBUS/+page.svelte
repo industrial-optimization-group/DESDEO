4681cf4f892b073fe4427972f41d22234fe57618
juropo <juho.o.roponen@jyu.fi>
2025-09-10 16:34:06 +0300

    maybe it will update correctly now

--- webui/src/routes/interactive_methods/NIMBUS/+page.svelte
+++ webui/src/routes/interactive_methods/NIMBUS/+page.svelte
@@ -200,7 +200,8 @@
 	let compensation = $state(0.0);
 
 	// Variables for  for showing that calculations are ongoing
-	let calculating = $state(false);
+	// svelte-ignore non_reactive_update
+	let calculating = false;
 	let error: string | null = null;
 
 	// Validation: iteration is allowed when at least one preference is better and one is worse than current objectives
