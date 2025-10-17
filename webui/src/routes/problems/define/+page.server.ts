49577223429a5dc3575643ad0606c4b7c5805849
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2025-10-17 15:56:33 +0300

    GUI changes (WIP).
    - Cookies are now sent properly, need to implement the refresh of
      cookies in hooks.servr.ts.

--- webui/src/routes/problems/define/+page.server.ts
+++ webui/src/routes/problems/define/+page.server.ts
@@ -382,8 +382,10 @@ export const actions: Actions = {
 
 import { addProblemJsonProblemAddJsonPost } from "$lib/gen/endpoints/dESDEOFastAPI";
 import { addProblemJsonProblemAddJsonPostBody } from "$lib/gen/endpoints/dESDEOFastAPIzod";
+import type { Actions } from "@sveltejs/kit";
 import { superValidate, fail } from 'sveltekit-superforms';
 import { zod4 } from 'sveltekit-superforms/adapters';
+import type { PageServerLoad } from "../$types";
 
 const problemAddJsonSchema = addProblemJsonProblemAddJsonPostBody;
 
@@ -394,18 +396,15 @@ export const load: PageServerLoad = async () => {
 }
 
 export const actions: Actions = {
-  upload_json: async ({ request }) => {
-    const form = await superValidate(request as typeof problemAddJsonSchema, zod4(problemAddJsonSchema));
-
+  upload_json: async ({ request, fetch }) => {
+    const form = await superValidate(request, zod4(problemAddJsonSchema));
 
     if (!form.valid) {
       return fail(400, { form });
     }
 
-    console.log(form.data.json_file);
-    const response = await addProblemJsonProblemAddJsonPost(form.data);
-
-    console.log(response);
+    // It is very important we use the fetch that is provided by the client (browser)
+    const response = await addProblemJsonProblemAddJsonPost(form.data, { fetchImpl: fetch } as RequestInit);
 
   }
-}
+};
