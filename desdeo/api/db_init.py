818f6ac5b61b45094ba9f95c9b7b92569580a4ef
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2025-11-06 16:09:40 +0200

    Web-API.
    - Modified the database init script to add the mobile clinic problem
      from a file.

--- desdeo/api/db_init.py
+++ desdeo/api/db_init.py
@@ -1,14 +1,22 @@
 """This module initializes the database."""
 
 import warnings
+from pathlib import Path
 
 from sqlalchemy_utils import database_exists
 from sqlmodel import Session, SQLModel
 
 from desdeo.api.config import ServerConfig, SettingsConfig
 from desdeo.api.db import engine
-from desdeo.api.models import ProblemDB, User, UserRole
+from desdeo.api.models import (
+    ProblemDB,
+    ProblemMetaDataDB,
+    SolverSelectionMetadata,
+    User,
+    UserRole,
+)
 from desdeo.api.routers.user_authentication import get_password_hash
+from desdeo.problem import Problem
 from desdeo.problem.testproblems import dtlz2, river_pollution_problem, simple_knapsack
 
 problems = [dtlz2(10, 3), simple_knapsack(), river_pollution_problem()]
@@ -45,6 +53,27 @@ if __name__ == "__main__":
 
             session.commit()
             session.refresh(problem_db)
+
+            # Add clinic problem
+            problem_path = Path("/home/kilo/Downloads/clinicOptProb_ideal_and_nadir.json")
+
+            problem = Problem.load_json(problem_path)
+            problem_db = ProblemDB.from_problem(problem, user_analyst)
+
+            session.add(problem_db)
+            session.commit()
+            session.refresh(problem_db)
+
+            metadata = ProblemMetaDataDB(problem_id=problem_db.id, problem=problem_db)
+            session.add(metadata)
+            session.commit()
+            session.refresh(metadata)
+
+            solver_metadata = SolverSelectionMetadata(
+                metadata_id=metadata.id, solver_string_representation="pyomo_ipopt", metadata_instance=metadata
+            )
+            session.add(solver_metadata)
+            session.commit()
 
         """
         db.add(user_analyst)
