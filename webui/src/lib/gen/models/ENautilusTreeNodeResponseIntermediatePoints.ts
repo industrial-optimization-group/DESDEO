f3a0f5afb5c90d94bbede07a2c16a922dfc94abe
Giovanni Misitano <giovanni.a.misitano@jyu.fi>
2026-03-26 16:02:41 +0200

    Web-GUI: update Orval

--- webui/src/lib/gen/models/ENautilusTreeNodeResponseIntermediatePoints.ts
+++ webui/src/lib/gen/models/ENautilusTreeNodeResponseIntermediatePoints.ts
@@ -6,6 +6,7 @@
  * OpenAPI spec version: 0.1.0
  */
 
-export type GetProblemJsonProblemProblemIdJsonGetParams = {
-	session_id?: number | null;
-};
+/**
+ * Intermediate points shown at this step.
+ */
+export type ENautilusTreeNodeResponseIntermediatePoints = { [key: string]: number }[] | null;
