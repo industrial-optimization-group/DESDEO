96a3ed0351c118e83d6269d8a488789e8afdec3f
juropo <juho.o.roponen@jyu.fi>
2026-04-10 19:26:29 +0300

    Gurobipy was never told if it should maximize an objective

--- desdeo/problem/gurobipy_evaluator.py
+++ desdeo/problem/gurobipy_evaluator.py
@@ -567,7 +567,7 @@ class GurobipyEvaluator:
         self.variables.pop(symbol)
         self.model.update()
 
-    def set_optimization_target(self, target: str, maximize: bool = False):
+    def set_optimization_target(self, target: str):
         """Sets a minimization objective to match the target objective or scalarization of the gurobipy model.
 
         Args:
@@ -578,7 +578,12 @@ class GurobipyEvaluator:
         Raises:
             GurobipyEvaluatorError: the given target was not an attribute of the gurobipy model.
         """
-        if not ((target in self.objective_functions) or (target in self.scalarizations)):
+        if target in self.objective_functions:
+            objective = self.problem.get_objective(symbol=target, copy=False)
+            maximize = False if objective is None else bool(objective.maximize)
+        elif target in self.scalarizations:
+            maximize = False
+        else:
             msg = f"The gurobipy model has no objective or scalarization named {target}."
             raise GurobipyEvaluatorError(msg)
 
