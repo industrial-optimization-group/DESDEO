6b2cc7f58cf3c567f838a542f61ab1a0d14f6fe2
Mastereeli <eelimaki2008@gmail.com>
2026-06-10 11:47:53 +0300

    Added the ZDT4 problem and tests for it.

--- desdeo/problem/testproblems/zdt_problem.py
+++ desdeo/problem/testproblems/zdt_problem.py
@@ -1,3 +1,5 @@
+"""Contains implementations of ZDT problems."""
+
 from math import pi
 
 from desdeo.problem.schema import (
@@ -269,6 +271,96 @@ def zdt3(
         objectives=objectives,
         extra_funcs=extras,
         is_convex=False,
+        is_linear=False,
+        is_twice_differentiable=True,
+    )
+
+
+def zdt4(number_of_variables: int) -> Problem:
+    r"""Defines the ZDT4 test problem.
+
+    The problem has a variable number of decision variables and two objective functions to be minimized as
+    follows:
+
+    \begin{align*}
+        \min\quad f_1(\textbf{x}) &= x_1 \\
+        \min\quad f_2(\text{x})\&=g(\text{x})\cdot\left(h\right) \\
+        g(\text{x})\&=1+10\left(n-1\right)+\sum_{i=2}^n\left[x_i^2-10\cos\left(4\pi\cdot x_i\right)\right] \\
+        h(f_1{,}g)\&=1-\sqrt{\frac{f_1}{g}}, \\
+    \end{align*}
+
+    where $f_1$ and $f_2$ are objective functions, $x_1,\dots,x_n$ are decision variables, $n$
+    is the number of decision variables,
+    and $g$ and $h$ are auxiliary functions.
+    """
+    n = number_of_variables
+
+    # function f_1
+    f1_symbol = "f_1"
+    f1_expr = "x_1"
+
+    # function g
+    g_symbol = "g"
+    g_expr_1 = f"1 + 10 * ({n} - 1)"
+    g_expr_2 = "(" + " + ".join([f"x_{i}**2 - 10 * Cos(4 * {pi} * x_{i})" for i in range(2, n + 1)]) + ")"
+    g_expr = g_expr_1 + " + " + g_expr_2
+
+    # function h(f, g)
+    h_symbol = "h"
+    h_expr = f"1 - Sqrt(({f1_expr}) / ({g_expr}))"
+
+    # function f_2
+    f2_symbol = "f_2"
+    f2_expr = f"{g_symbol} * {h_symbol}"
+
+    variables = [
+        Variable(name=f"x_{1}", symbol=f"x_{1}", variable_type="real", lowerbound=0, upperbound=1, initial_value=0.5),
+    ] + [
+        Variable(name=f"x_{i}", symbol=f"x_{i}", variable_type="real", lowerbound=-5, upperbound=5, initial_value=0)
+        for i in range(2, n + 1)
+    ]
+
+    objectives = [
+        Objective(
+            name="f_1",
+            symbol=f1_symbol,
+            func=f1_expr,
+            maximize=False,
+            ideal=0,
+            nadir=1,
+            is_convex=True,
+            is_linear=True,
+            is_twice_differentiable=True,
+        ),
+        Objective(
+            name="f_2",
+            symbol=f2_symbol,
+            func=f2_expr,
+            maximize=False,
+            ideal=0,
+            nadir=1,
+            is_convex=True,
+            is_linear=False,
+            is_twice_differentiable=True,
+        ),
+    ]
+
+    extras = [
+        ExtraFunction(
+            name="g", symbol=g_symbol, func=g_expr, is_convex=True, is_linear=True, is_twice_differentiable=True
+        ),
+        ExtraFunction(
+            name="h", symbol=h_symbol, func=h_expr, is_convex=True, is_linear=False, is_twice_differentiable=True
+        ),
+    ]
+
+    return Problem(
+        name="zdt4",
+        description="The ZDT4 test problem.",
+        variables=variables,
+        objectives=objectives,
+        extra_funcs=extras,
+        is_convex=True,
         is_linear=False,
         is_twice_differentiable=True,
     )
