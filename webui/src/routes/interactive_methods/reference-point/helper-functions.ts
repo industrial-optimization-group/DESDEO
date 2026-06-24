6bd9f3e1867b1755f3e67e65bd50afe500fdb212
Luka Rantalainen <luka.j.rantalainen@gmail.com>
2026-06-24 15:07:53 +0300

    added missing import

--- webui/src/routes/interactive_methods/reference-point/helper-functions.ts
+++ webui/src/routes/interactive_methods/reference-point/helper-functions.ts
@@ -14,6 +14,7 @@ import type {
 	SolutionReferenceResponse,
 	RPMState
 } from '$lib/gen/endpoints/DESDEOFastAPI';
+
 import type { RPMPageState } from './types';
 
 // Type definitions for NIMBUS components
