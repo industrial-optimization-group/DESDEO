c1ef813b59e15bfa05765d686c09ae853fc9b2ff
Juho Roponen <juho.o.roponen@jyu.fi>
2026-06-30 14:34:00 +0300

    Updated mcdm init

--- desdeo/mcdm/__init__.py
+++ desdeo/mcdm/__init__.py
@@ -9,20 +9,28 @@ __all__ = [
     "calculate_intermediate_points",
     "calculate_lower_bounds",
     "calculate_reachable_subset",
+    "cumulus_generate_starting_point",
+    "cumulus_infer_classifications",
+    "cumulus_solve_intermediate_solutions",
+    "cumulus_solve_sub_problems",
     "enautilus_get_representative_solutions",
     "enautilus_step",
-    "generate_starting_point",
-    "infer_classifications",
+    "nimbus_generate_starting_point",
+    "nimbus_infer_classifications",
+    "nimbus_solve_intermediate_solutions",
+    "nimbus_solve_sub_problems",
     "prune_by_average_linkage",
     "rpm_intermediate_solutions",
     "rpm_solve_solutions",
     "solve_group_sub_problems",
-    "solve_intermediate_solutions",
-    "solve_sub_problems",
     "voting_procedure",
 ]
 
 from .cumulus import CumulusError, CumulusScalarization
+from .cumulus import generate_starting_point as cumulus_generate_starting_point
+from .cumulus import infer_classifications as cumulus_infer_classifications
+from .cumulus import solve_intermediate_solutions as cumulus_solve_intermediate_solutions
+from .cumulus import solve_sub_problems as cumulus_solve_sub_problems
 from .enautilus import (
     ENautilusResult,
     calculate_closeness,
@@ -34,11 +42,9 @@ from .enautilus import (
     prune_by_average_linkage,
 )
 from .gnimbus import solve_group_sub_problems, voting_procedure
-from .nimbus import (
-    NimbusError,
-    generate_starting_point,
-    infer_classifications,
-    solve_intermediate_solutions,
-    solve_sub_problems,
-)
+from .nimbus import NimbusError
+from .nimbus import generate_starting_point as nimbus_generate_starting_point
+from .nimbus import infer_classifications as nimbus_infer_classifications
+from .nimbus import solve_intermediate_solutions as nimbus_solve_intermediate_solutions
+from .nimbus import solve_sub_problems as nimbus_solve_sub_problems
 from .reference_point_method import rpm_intermediate_solutions, rpm_solve_solutions
